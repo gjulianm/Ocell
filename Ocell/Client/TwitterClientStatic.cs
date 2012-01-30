3af18eced8f40b98fbd2e1b51a47d21d5aa80cad
Guillermo Julián <gjulian93@gmail.com>
2012-01-30 23:47:05 +0100

    Added support for multiple services.

--- Ocell/Client/TwitterClientStatic.cs
+++ Ocell/Client/TwitterClientStatic.cs
@@ -1,10 +1,25 @@
 ﻿using TweetSharp;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
 
 namespace Ocell
 {
     public static class Clients
     {
-        public static TwitterService Service;
+        public static TwitterService Service {
+            get {
+                return Services.First();
+            }
+
+            set {
+                if(Services.Count > 0)
+                    Services[0] = value;
+                else
+                    Services.Add(value);
+            }
+        }
+        public static List<TwitterService> Services = new List<TwitterService>();
         public static bool isServiceInit = false;
         public static string ScreenName;
         public static int Uid;
