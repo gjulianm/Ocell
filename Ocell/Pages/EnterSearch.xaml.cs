02f2c717ea967194f8b298857b7397ccbc9c51c8
Guillermo Julián <gjulian93@gmail.com>
2012-03-04 23:17:56 +0100

    Added Search button from MainPage.

---
+++ Ocell/Pages/EnterSearch.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace Ocell
+{
+    public partial class EnterSearch : PhoneApplicationPage
+    {
+        public EnterSearch()
+        {
+            InitializeComponent();
+            ThemeFunctions.ChangeBackgroundIfLightTheme(LayoutRoot);
+        }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+        	NavigationService.Navigate(new Uri("/Pages/Search.xaml?q=" + SearchQuery.Text, UriKind.Relative));
+        }
+    }
+}
