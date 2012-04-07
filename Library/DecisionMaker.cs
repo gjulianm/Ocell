f5a9fa2d7d7b03920d25d5bde7067bff6dac86a2
Guillermo Julián <gjulian93@gmail.com>
2012-04-07 19:20:06 +0200

    Updated DecisionMaker for new caché scenario.

--- Library/DecisionMaker.cs
+++ Library/DecisionMaker.cs
@@ -16,7 +16,7 @@ namespace Ocell.Library
 {
     public static class DecisionMaker
     {
-        private static int GetAvgTimeBetweenTweets(ref IEnumerable<TwitterStatus> Tweets)
+        public static int GetAvgTimeBetweenTweets(IEnumerable<TwitterStatus> Tweets)
         {
             int i;
             double sum = 0;
@@ -33,14 +33,31 @@ namespace Ocell.Library
             return ((int)sum) / TweetsToAnalyze;
         }
 
+        public static int GetAvgTimeBetweenTweets(IEnumerable<ITweetable> Tweets)
+        {
+            int i;
+            double sum = 0;
+            TimeSpan Difference;
+            const int TweetsToAnalyze = 30;
+            int upperLimit = Math.Min(TweetsToAnalyze, Tweets.Count()) - 1;
+
+            for (i = 0; i < upperLimit; i++)
+            {
+                Difference = Tweets.ElementAt(i).CreatedDate - Tweets.ElementAt(i + 1).CreatedDate;
+                sum += Difference.TotalSeconds;
+            }
+
+            return ((int)sum) / TweetsToAnalyze;
+        }
+
         public static bool ShouldLoadCache(ref IEnumerable<TwitterStatus> List)
         {
-            int AverageTimeBetweenTweets;
+            /*int AverageTimeBetweenTweets;
             int CurrentDifference;
             /*
              * Supposing we get ~20 tweets per time, this is an acceptable value
              *  so the user does not lose too many tweets.
-             */
+             *
             int MaxTimesDifference = 20;
 
             if(List == null || List.Count() == 0)
@@ -49,10 +66,13 @@ namespace Ocell.Library
             if (!System.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable())
                 return true;
 
-            AverageTimeBetweenTweets = GetAvgTimeBetweenTweets(ref List);
+            AverageTimeBetweenTweets = GetAvgTimeBetweenTweets(List);
             CurrentDifference = (int)Math.Abs((DateTime.Now.ToUniversalTime() - List.First().CreatedDate).TotalSeconds);
 
-            return CurrentDifference < MaxTimesDifference * AverageTimeBetweenTweets;
+            return CurrentDifference < MaxTimesDifference * AverageTimeBetweenTweets;*/
+
+            // ALWAYS load cache.
+            return true;
  
         }
     }
