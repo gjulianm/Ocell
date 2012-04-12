72c57f168f62e5f2d4ee91c69077805405177615
Guillermo Julián <gjulian93@gmail.com>
2012-04-12 23:19:23 +0200

    Added UserProvider and stop methods.

--- Library/UsernameProvider.cs
+++ Library/UsernameProvider.cs
@@ -1,24 +1,92 @@
 ﻿using System;
-using System.Net;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Ink;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Net;
 using TweetSharp;
-using System.Linq;
 
 namespace Ocell.Library.Twitter
 {
+    public class UserProvider
+    {
+        public ObservableCollection<TwitterUser> Users { get; set; }
+        public UserToken User { get; set; }
+        public bool GetFollowers { get; set; }
+        public bool GetFollowing { get; set; }
+        private TwitterService _service;
+
+        public UserProvider()
+        {
+            GetFollowers = true;
+            GetFollowing = true;
+            Users = new ObservableCollection<TwitterUser>();
+        }
+
+        private void GetService()
+        {
+            if (User == null)
+                _service = ServiceDispatcher.GetDefaultService();
+            else
+                _service = ServiceDispatcher.GetService(User);
+        }
+
+        public void Start()
+        {
+            if (_service == null)
+                GetService();
+
+            if(GetFollowing)
+                _service.ListFriends(-1, ReceiveList);
+            if (GetFollowers)
+                _service.ListFollowers(-1, ReceiveList);
+        }
+
+        private void ReceiveList(TwitterCursorList<TwitterUser> users, TwitterResponse response)
+        {
+            bool finish = false;
+            if (users == null || response.StatusCode != HttpStatusCode.OK)
+            {
+                if (Error != null)
+                    Error(this, response);
+            }
+
+            if (users.NextCursor != null && users.NextCursor != 0 && _service != null)
+            {
+                if (GetFollowing)
+                    _service.ListFriends((long)users.NextCursor, ReceiveList);
+                if (GetFollowers)
+                    _service.ListFollowers((long)users.NextCursor, ReceiveList);
+            }
+            else
+                finish = true;
+
+            if (Users == null)
+                Users = new ObservableCollection<TwitterUser>();
+
+            foreach (var user in users)
+                Users.Add(user);
+
+            if (finish && Finished != null)
+                Finished(this, new EventArgs());
+        }
+
+        public delegate void OnError(object sender, TwitterResponse response);
+        public event OnError Error;
+
+        public event EventHandler Finished;
+    }
+
     public class UsernameProvider
     {
         public IList<string> Usernames { get; protected set; }
         public UserToken User { get; set; }
         private TwitterService _service;
+        private bool _stop;
+
+        public UsernameProvider()
+        {
+            Usernames = new List<string>();
+            _stop = false;
+        }
 
         private void GetService()
         {
@@ -42,6 +110,13 @@ namespace Ocell.Library.Twitter
             {
                 if (Error != null)
                     Error(this, response);
+                return;
+            }
+
+            if (_stop)
+            {
+                _stop = false;
+                return;
             }
 
             if (friends.NextCursor != null && friends.NextCursor != 0 && _service != null)
@@ -52,6 +127,11 @@ namespace Ocell.Library.Twitter
 
             foreach (var user in friends)
                 Usernames.Add(user.ScreenName);
+        }
+
+        public void Stop()
+        {
+            _stop = true;
         }
 
         public delegate void OnError(object sender, TwitterResponse response);
