affdd2068bfdf71a9a5eb006a794f7c98bfdf716
Guillermo Julián <gjulian93@gmail.com>
2012-04-12 23:24:07 +0200

    Changed to use UserProvider.

--- Ocell/Pages/SelectUser.xaml.cs
+++ Ocell/Pages/SelectUser.xaml.cs
@@ -8,12 +8,15 @@ using Microsoft.Phone.Controls;
 using Ocell.Library;
 using Ocell.Library.Twitter;
 using TweetSharp;
+using System.Collections.ObjectModel;
 
 namespace Ocell.Pages
 {
     public partial class SelectUser : PhoneApplicationPage
     {
         public CollectionViewSource Source;
+        private UserProvider _provider;
+        private ObservableCollection<TwitterUser> _users;
 
         public SelectUser()
         {
@@ -28,9 +31,41 @@ namespace Ocell.Pages
 
             AccountsPicker.DataContext = Config.Accounts;
             AccountsPicker.ItemsSource = Config.Accounts;
-            
+
+            if(_provider == null)
+                _provider = new UserProvider();
+            _users = new ObservableCollection<TwitterUser>();
+            _provider.GetFollowers = true;
+            _provider.GetFollowing = false;
+            _provider.Users.CollectionChanged += new System.Collections.Specialized.NotifyCollectionChangedEventHandler(Users_CollectionChanged);
+            Source.Source = _users;
+            Source.SortDescriptions.Add(new System.ComponentModel.SortDescription("ScreenName", System.ComponentModel.ListSortDirection.Ascending));
+
             UserList.DataContext = Source;
             UserList.ItemsSource = Source.View;
+        }
+
+        void Users_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            if (e.NewItems != null && e.NewItems.Count > 0)
+            {
+                Dispatcher.BeginInvoke(() =>
+                {
+                    foreach (var item in e.NewItems.Cast<TwitterUser>())
+                        if (!_users.Contains(item))
+                            _users.Add(item);
+                });
+            }
+
+            if (e.OldItems != null && e.OldItems.Count > 0)
+            {
+                Dispatcher.BeginInvoke(() =>
+                {
+                    foreach (var item in e.OldItems.Cast<TwitterUser>())
+                        if (_users.Contains(item))
+                            _users.Remove(item);
+                });
+            }
         }
 
         void UserFilter_TextChanged(object sender, TextChangedEventArgs e)
@@ -60,31 +95,19 @@ namespace Ocell.Pages
 
         void GetUsersFor(UserToken User)
         {
-            TwitterService Service;
-            Service = ServiceDispatcher.GetService(User);
-            pBar.IsVisible = true;
-            pBar.Text = "Downloading list of users...";
-            Service.ListFollowers(ReceiveUsers);
+            if (_provider == null)
+                _provider = new UserProvider();
+            Dispatcher.BeginInvoke(() => { pBar.IsVisible = true; pBar.Text = "Downloading list of users..."; });
+            _provider.User = User;
+            _provider.GetFollowers = true;
+            _provider.GetFollowing = false;
+            _provider.Finished += new EventHandler(_provider_Finished);
+            _provider.Start();
         }
 
-        void ReceiveUsers(TwitterCursorList<TwitterUser> Users, TwitterResponse Response)
+        void _provider_Finished(object sender, EventArgs e)
         {
-            Dispatcher.BeginInvoke(() =>
-            {
-                pBar.IsVisible = false;
-            });
-            if (Response.StatusCode == HttpStatusCode.OK)
-            {
-                Dispatcher.BeginInvoke(() =>
-                {
-                    Source.Source = Users.ToList();
-                    UserList.ItemsSource = Source.View;
-                });
-            }
-            else
-            {
-                Dispatcher.BeginInvoke(() => MessageBox.Show("There was a problem trying to get the list of users.")) ;
-            }
+            Dispatcher.BeginInvoke(() => pBar.IsVisible = false);
         }
 
         private void Next_Click(object sender, System.EventArgs e)
