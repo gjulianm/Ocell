13713e1cedc247fa12c55741a52e123ff236632d
Guillermo Julián <gjulian93@gmail.com>
2012-05-04 20:38:11 +0200

    Added some icons and TimeTracker class to track execution time.

---
+++ Library/Watch.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace Ocell.Library
+{
+    public static class TimeTracker
+    {
+        public static void Track(Action function)
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            function();
+            watch.Stop();
+            Debug.WriteLine("Method " + function.Method.Name +" took " + watch.ElapsedMilliseconds + " ms.");
+        }
+    }
+}
