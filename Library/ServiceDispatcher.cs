76f87ea7e7edb0d55a8291c2c23fa11f2bee65e0
Guillermo Julián <gjulian93@gmail.com>
2012-05-26 20:40:58 +0200

    Thread safety.

--- Library/ServiceDispatcher.cs
+++ Library/ServiceDispatcher.cs
@@ -6,22 +6,31 @@ namespace Ocell.Library.Twitter
 {
     public static class ServiceDispatcher 
     {
+        private static object _lockFlag = new object();
         private static Dictionary<string, TwitterService> _list;
         
         public static TwitterService GetService(UserToken account)
         {
-            if(_list == null)
-                _list = new Dictionary<string, TwitterService>();
+            lock (_lockFlag)
+            {
+                if (_list == null)
+                    _list = new Dictionary<string, TwitterService>();
+            }
 
             if (account == null || account.Key == null)
                 return null;
 
-            if(_list.ContainsKey(account.Key))
-                return _list[account.Key];
+            lock (_lockFlag)
+            {
+                if (_list.ContainsKey(account.Key))
+                    return _list[account.Key];
+            }
             
             TwitterService srv = new TwitterService();
             srv.AuthenticateWith(SensitiveData.ConsumerToken, SensitiveData.ConsumerSecret, account.Key, account.Secret);
-            _list.Add(account.Key, srv);
+
+            lock(_lockFlag)
+                _list.Add(account.Key, srv);
             return srv;
         }
 
