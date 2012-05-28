a5d0c8319db06369bb28d65ee88fbe8ef03ab3c6
Guillermo Julián <gjulian93@gmail.com>
2012-05-28 20:09:57 +0200

    MVVM to User.xaml.

--- Ocell/Pages/Elements/User.xaml.cs
+++ Ocell/Pages/Elements/User.xaml.cs
@@ -18,322 +18,43 @@ namespace Ocell.Pages.Elements
 {
     public partial class User : PhoneApplicationPage
     {
-        public TwitterUser CurrentUser;
-        private bool follows;
-        private bool selectionChangeFired = false;
-        ApplicationBarIconButton followBtn;
-        ApplicationBarIconButton pinBtn;
-        ApplicationBarMenuItem changeAvatarItem;
-        private TwitterService _srv;
-        private UserToken _account;
+        UserModel viewModel;
 
         public User()
         {
             InitializeComponent();
             ThemeFunctions.ChangeBackgroundIfLightTheme(LayoutRoot);
 
-            this.Loaded += new RoutedEventHandler(User_Loaded);
-            TweetList.SelectionChanged += new SelectionChangedEventHandler(ListBox_SelectionChanged);
-            TweetList.Loader.Cached = false;
-            _account = Config.Accounts.FirstOrDefault();
-            if (_account == null)
-                NavigationService.Navigate(Uris.MainPage);
-            _srv = ServiceDispatcher.GetService(_account);
-        }
+            viewModel = new UserModel();
+            DataContext = viewModel;
 
-        void User_Loaded(object sender, RoutedEventArgs e)
-        {
-            string userName;
-            if (!NavigationContext.QueryString.TryGetValue("user", out userName))
-                NavigationService.GoBack();
+            this.Loaded += (sender, e) =>
+                {
+                    string userName;
+                    if (!NavigationContext.QueryString.TryGetValue("user", out userName))
+                    {
+                        NavigationService.GoBack();
+                        return;
+                    }
+                    viewModel.Loaded(userName);
+                };
 
-            Regex remove = new Regex("@|:");
-            userName = remove.Replace(userName, "");
-
-            Dispatcher.BeginInvoke(() => { pBar.IsVisible = true; pBar.Text = "Retrieving profile..."; });
-            _srv.ListUserProfilesFor(new List<string> { userName }, GetUser);
-            pvProfile.DataContext = CurrentUser;
-            if (followBtn == null)
+            viewModel.PropertyChanged += (sender, e) =>
             {
-                followBtn = new ApplicationBarIconButton();
-                followBtn.Text = "follow";
-                followBtn.IconUri = new Uri("/Images/Icons_White/appbar.minus.rest.png", UriKind.Relative);
-                followBtn.Click += new EventHandler(followBtn_Click);
-                followBtn.IsEnabled = false;
-                ApplicationBar.Buttons.Add(followBtn);
-            }
-
-            if (pinBtn == null)
-            {
-                pinBtn = new ApplicationBarIconButton();
-                pinBtn.Text = "pin to main page";
-                pinBtn.IconUri = new Uri("/Images/Icons_White/appbar.pin.rest.png", UriKind.Relative);
-                pinBtn.Click += new EventHandler(pinBtn_Click);
-                pinBtn.IsEnabled = false;
-                ApplicationBar.Buttons.Add(pinBtn);
-            }
-        }
-
-        void pinBtn_Click(object sender, EventArgs e)
-        {
-            TwitterResource resource = new TwitterResource {
-                Type = ResourceType.Tweets,
-                Data = CurrentUser.ScreenName,
-                User = DataTransfer.CurrentAccount
+                if (e.PropertyName == "ScreenName")
+                {
+                    TweetList.Bind(new TwitterResource { String = "Tweets:" + viewModel.ScreenName, User = DataTransfer.CurrentAccount });
+                    TweetList.Loader.Load();
+                }
             };
 
-            // Don't care about the User property of the TwitterResource.
-            if(Config.Columns.Any(item => item != null && item.Type == ResourceType.Tweets && item.Data == CurrentUser.ScreenName))
-            {
-                Dispatcher.BeginInvoke(() => MessageBox.Show("That user is already pinned!"));
-                return;
-            }
-
-            Config.Columns.Add(resource);
-            Config.SaveColumns();
-            DataTransfer.ShouldReloadColumns = true;
-            Dispatcher.BeginInvoke(() => Notificator.ShowMessage("User succesfully pinned!", pBar));
-        }
-
-        void GetUser(IEnumerable<TwitterUser> user, TwitterResponse response)
-        {
-            if (response.StatusCode != HttpStatusCode.OK || user == null || user.Count() == 0)
-            {
-                Dispatcher.BeginInvoke(() =>
+            TweetList.Loader.PropertyChanged += (sender, e) =>
                 {
-                    MessageBox.Show("The user could not be retrieved.");
-                    pBar.IsVisible = false;
-                    pBar.Text = "";
-                    NavigationService.GoBack();
-                });
-                return;
-            }
+                    if (e.PropertyName == "IsLoading")
+                        viewModel.IsLoading = TweetList.Loader.IsLoading;
+                };
 
-            CurrentUser = user.First();
-            Dispatcher.BeginInvoke(() =>
-            {
-                pinBtn.IsEnabled = true;
-
-                FullName.Text = CurrentUser.Name;
-                ScreenName.Text = "@" + CurrentUser.ScreenName;
-                Avatar.Source = new BitmapImage(new Uri(CurrentUser.ProfileImageUrl, UriKind.Absolute));
-
-                if (!string.IsNullOrWhiteSpace(CurrentUser.Url))
-                {
-                    Website.Content = CurrentUser.Url;
-                    Website.NavigateUri = new Uri(CurrentUser.Url, UriKind.Absolute);
-                }
-
-                Biography.Text = CurrentUser.Description;
-                Followers.Text = CurrentUser.FollowersCount.ToString();
-                Following.Text = CurrentUser.FriendsCount.ToString();
-                Tweets.Text = CurrentUser.StatusesCount.ToString();
-                pBar.IsVisible = false;
-                pBar.Text = "";
-
-                TweetList.Bind(new TwitterResource { Data = CurrentUser.ScreenName, Type = ResourceType.Tweets, User = DataTransfer.CurrentAccount });
-                TweetList.Loader.Load();
-            });
-
-            _srv.GetFriendshipInfo((int)_account.Id, CurrentUser.Id, GetFriendship);
-            CheckForAvatarChange();
-        }
-
-        private void CheckForAvatarChange()
-        {
-            if (Config.Accounts.Any(item => item.Id == CurrentUser.Id) && changeAvatarItem == null)
-            {
-                changeAvatarItem = new ApplicationBarMenuItem();
-                changeAvatarItem.IsEnabled = true;
-                changeAvatarItem.Text = "change profile image";
-                changeAvatarItem.Click += new EventHandler(ChangeAvatar);
-                Dispatcher.BeginInvoke(() => ApplicationBar.MenuItems.Add(changeAvatarItem));
-            }
-        }
-
-        void ChangeAvatar(object sender, EventArgs e)
-        {
-            PhotoChooserTask task = new PhotoChooserTask();
-            task.ShowCamera = true;
-            task.Completed += new EventHandler<PhotoResult>(task_Completed);
-            task.Show();
-        }
-
-        void task_Completed(object sender, PhotoResult e)
-        {
-            UserToken user;
-            user = Config.Accounts.FirstOrDefault(item => item != null && item.ScreenName == CurrentUser.ScreenName);
-            if (e.TaskResult == TaskResult.OK && user != null)
-            {
-                Dispatcher.BeginInvoke(() => { pBar.IsVisible = true; pBar.Text = "Uploading picture..."; });
-                TwitterService srv = ServiceDispatcher.GetService(user);
-                srv.UpdateProfileImage(e.OriginalFileName, e.ChosenPhoto, ReceivePhotoUpload);
-            }
-        }
-
-        private void ReceivePhotoUpload(TwitterUser user, TwitterResponse response)
-        {
-            Dispatcher.BeginInvoke(() => { pBar.IsVisible = false; pBar.Text = ""; });
-            if (response.StatusCode == HttpStatusCode.OK)
-                Dispatcher.BeginInvoke(() => MessageBox.Show("Your profile image has been changed."));
-            else
-                Dispatcher.BeginInvoke(() => MessageBox.Show("An error happened while uploading your image. Verify the file is less than 700 kB of size."));
-        }
-
-        private void GetFriendship(TwitterFriendship friendship, TwitterResponse response)
-        {
-            if (response.StatusCode != HttpStatusCode.OK || followBtn == null)
-                return;
-
-
-            followBtn.IsEnabled = true;
-
-            if (friendship.Relationship.Source.Following)
-                Dispatcher.BeginInvoke(() =>
-                {
-                    followBtn.Text = "unfollow";
-                    followBtn.IconUri = new Uri("/Images/Icons_White/appbar.minus.rest.png", UriKind.Relative);
-                    follows = true;
-                });
-            else
-                Dispatcher.BeginInvoke(() =>
-                {
-                    followBtn.Text = "follow";
-                    followBtn.IconUri = new Uri("/Images/Icons_White/appbar.add.rest.png", UriKind.Relative);
-                    follows = true;
-                });
-
-        }
-
-        private void ListBox_Loaded(object sender, RoutedEventArgs e)
-        {
-            ExtendedListBox list = sender as ExtendedListBox;
-            TwitterResource Resource = new TwitterResource();
-
-            if (list == null)
-                return;
-
-            if (list.Tag != null && list.Tag is string && CurrentUser != null)
-            {
-                if ((string)list.Tag == "Search")
-                {
-                    Resource.Type = ResourceType.Search;
-                    Resource.Data = "@" + CurrentUser.ScreenName;
-                }
-                else if ((string)list.Tag == "Tweets")
-                {
-                    Resource.Type = ResourceType.Tweets;
-                    Resource.Data = CurrentUser.ScreenName;
-                }
-                list.Bind(Resource);
-            }
-
-            list.Compression += new ExtendedListBox.OnCompression(list_Compression);
-            list.Loader.Error += new TweetLoader.OnError(Loader_Error);
-            list.Loader.LoadFinished += new EventHandler(Loader_LoadFinished);
-        }
-        void Loader_LoadFinished(object sender, EventArgs e)
-        {
-            Dispatcher.BeginInvoke(() => pBar.IsVisible = false);
-        }
-
-        void Loader_Error(TwitterResponse response)
-        {
-            Dispatcher.BeginInvoke(() =>
-            {
-                MessageBox.Show("Error loading tweets: " + response.StatusDescription);
-                pBar.IsVisible = false;
-            });
-        }
-
-        void list_Compression(object sender, CompressionEventArgs e)
-        {
-            bool Old = (e.Type == CompressionType.Bottom);
-            ExtendedListBox List = sender as ExtendedListBox;
-
-            if (List == null || List.Loader == null)
-                return;
-            Dispatcher.BeginInvoke(() => pBar.IsVisible = true);
-            List.Loader.Load(Old);
-        }
-
-        private void Website_Tap(object sender, System.Windows.Input.GestureEventArgs e)
-        {
-            WebBrowserTask browser = new WebBrowserTask();
-            browser.Uri = (sender as HyperlinkButton).NavigateUri;
-            browser.Show();
-        }
-
-        private void followBtn_Click(object sender, EventArgs e)
-        {
-            Dispatcher.BeginInvoke(() => pBar.IsVisible = true);
-            if (CurrentUser == null)
-                return;
-            if (follows)
-                ServiceDispatcher.GetCurrentService().UnfollowUser(CurrentUser.Id, Receive);
-            else
-                ServiceDispatcher.GetCurrentService().FollowUser(CurrentUser.Id, Receive);
-        }
-
-        private void Receive(TwitterUser user, TwitterResponse response)
-        {
-            if (response.StatusCode != HttpStatusCode.OK)
-                Dispatcher.BeginInvoke(() => MessageBox.Show("Error :("));
-            Dispatcher.BeginInvoke(() => pBar.IsVisible = false);
-            ServiceDispatcher.GetCurrentService().GetFriendshipInfo((int)DataTransfer.CurrentAccount.Id, CurrentUser.Id, GetFriendship);
-        }
-
-        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if (!selectionChangeFired)
-            {
-                DataTransfer.Status = e.AddedItems[0] as TwitterStatus;
-                ListBox list = sender as ListBox;
-                selectionChangeFired = true;
-                list.SelectedIndex = -1;
-
-                NavigationService.Navigate(Uris.ViewTweet);
-            }
-            else
-                selectionChangeFired = false;
-        }
-
-        private void spamBtn_Click(object sender, System.EventArgs e)
-        {
-            ServiceDispatcher.GetCurrentService().ReportSpam(CurrentUser.Id, (user, response) =>
-            {
-                if (response.StatusCode == HttpStatusCode.OK)
-                    Dispatcher.BeginInvoke(() => Notificator.ShowMessage("Blocked and reported user.", pBar));
-                else
-                    Dispatcher.BeginInvoke(() => Notificator.ShowMessage("User could not be reported.", pBar));
-            });
-        }
-
-        private void blockBtn_Click(object sender, System.EventArgs e)
-        {
-            ServiceDispatcher.GetCurrentService().BlockUser(CurrentUser.Id, (user, response) =>
-            {
-                if (response.StatusCode == HttpStatusCode.OK)
-                    Dispatcher.BeginInvoke(() => Notificator.ShowMessage("Blocked user.", pBar));
-                else
-                    Dispatcher.BeginInvoke(() => Notificator.ShowMessage("User could not be blocked.", pBar));
-            });
-        }
-
-        private void ManageLists_Click(object sender, System.EventArgs e)
-        {
-            Dispatcher.BeginInvoke(() => NavigationService.Navigate(new Uri("/Pages/Lists/ListManager.xaml?user=" + CurrentUser.ScreenName, UriKind.Relative)));
-        }
-
-        private void Avatar_Tap(object sender, System.Windows.Input.GestureEventArgs e)
-        {
-            
-            if (CurrentUser == null)
-                return;
-
-            WebBrowserTask wb = new WebBrowserTask();
-            wb.Uri = new Uri(CurrentUser.ProfileImageUrl, UriKind.Absolute);
-            wb.Show();
+            TweetList.Loader.Cached = false;
         }
     }
 }
---
+++ Ocell/Pages/Elements/UserModel.cs
@@ -0,0 +1,382 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using DanielVaughan.ComponentModel;
+using DanielVaughan;
+using DanielVaughan.Windows;
+using Ocell.Library;
+using System.Collections.Generic;
+using Ocell.Library.Twitter;
+using System.Collections.ObjectModel;
+using System.Text.RegularExpressions;
+using TweetSharp;
+using Ocell.Library;
+using System.Linq;
+using Microsoft.Phone.Tasks;
+namespace Ocell.Pages.Elements
+{
+    public class UserModel : ViewModelBase
+    {
+        TwitterUser user;
+
+        bool isLoading;
+        public bool IsLoading
+        {
+            get { return isLoading; }
+            set { Assign("IsLoading", ref isLoading, value); }
+        }
+
+        string barText;
+        public string BarText
+        {
+            get { return barText; }
+            set { Assign("BarText", ref barText, value); }
+        }
+
+        bool followed;
+        public bool Followed
+        {
+            get { return followed; }
+            set { Assign("Followed", ref followed, value); }
+        }
+
+        bool blocked;
+        public bool Blocked
+        {
+            get { return blocked; }
+            set { Assign("Blocked", ref blocked, value); }
+        }
+
+        bool isOwner;
+        public bool IsOwner
+        {
+            get { return isOwner; }
+            set { Assign("IsOwner", ref isOwner, value); }
+        }
+
+        #region Fields.
+        string avatar;
+        public string Avatar
+        {
+            get { return avatar; }
+            set { Assign("Avatar", ref avatar, value); }
+        }
+
+        string name;
+        public string Name
+        {
+            get { return name; }
+            set { Assign("Name", ref name, value); }
+        }
+
+        string screenName;
+        public string ScreenName
+        {
+            get { return screenName; }
+            set { Assign("ScreenName", ref screenName, value); }
+        }
+
+        string website;
+        public string Website
+        {
+            get { return website; }
+            set { Assign("Website", ref website, value); }
+        }
+
+        string biography;
+        public string Biography
+        {
+            get { return biography; }
+            set { Assign("Biography", ref biography, value); }
+        }
+
+        string tweets;
+        public string Tweets
+        {
+            get { return tweets; }
+            set { Assign("Tweets", ref tweets, value); }
+        }
+
+        string following;
+        public string Following
+        {
+            get { return following; }
+            set { Assign("Following", ref following, value); }
+        }
+
+        string followers;
+        public string Followers
+        {
+            get { return followers; }
+            set { Assign("Followers", ref followers, value); }
+        }
+
+        bool websiteEnabled;
+        public bool WebsiteEnabled
+        {
+            get { return websiteEnabled; }
+            set { Assign("WebsiteEnabled", ref websiteEnabled, value); }
+        }
+        #endregion
+
+        #region Commands
+        Func<object, bool> GenericCanExecute;
+
+        DelegateCommand followUser;
+        public ICommand FollowUser
+        {
+            get { return followUser; }
+        }
+
+        DelegateCommand unfollowUser;
+        public ICommand UnfollowUser
+        {
+            get { return unfollowUser; }
+        }
+
+        DelegateCommand pinUser;
+        public ICommand PinUser
+        {
+            get { return pinUser; }
+        }
+
+        DelegateCommand block;
+        public ICommand Block
+        {
+            get { return block; }
+        }
+
+        DelegateCommand unblock;
+        public ICommand Unblock
+        {
+            get { return unblock; }
+        }
+
+        DelegateCommand reportSpam;
+        public ICommand ReportSpam
+        {
+            get { return reportSpam; }
+        }
+
+        DelegateCommand manageLists;
+        public ICommand ManageLists
+        {
+            get { return manageLists; }
+        }
+
+        DelegateCommand changeAvatar;
+        public ICommand ChangeAvatar
+        {
+            get { return changeAvatar; }
+        }
+        #endregion
+
+        public UserModel()
+            : base("User")
+        {
+            GenericCanExecute = (obj) => user != null && DataTransfer.CurrentAccount != null;
+            followUser = new DelegateCommand((obj) =>
+            {
+                IsLoading = true;
+                ServiceDispatcher.GetService(DataTransfer.CurrentAccount).FollowUser(user.Id, ReceiveFollow);
+            }, GenericCanExecute);
+
+            unfollowUser = new DelegateCommand((obj) =>
+            {
+                IsLoading = true;
+                ServiceDispatcher.GetService(DataTransfer.CurrentAccount).UnfollowUser(user.Id, ReceiveFollow);
+            }, GenericCanExecute);
+
+            pinUser = new DelegateCommand((obj) =>
+                {
+                    Config.Columns.Add(new TwitterResource
+                    {
+                        Data = user.ScreenName,
+                        Type = ResourceType.Tweets,
+                        User = DataTransfer.CurrentAccount
+                    });
+                    Config.SaveColumns();
+                    MessageService.ShowLightNotification("User succesfully pinned.");
+                    pinUser.RaiseCanExecuteChanged();
+
+                }, item => GenericCanExecute.Invoke(null) 
+                    && !Config.Columns.Any(o => o.Type == ResourceType.Tweets && o.Data == user.ScreenName));
+
+            block = new DelegateCommand((obj) =>
+                {
+                    IsLoading = true;
+                    ServiceDispatcher.GetService(DataTransfer.CurrentAccount).BlockUser(user.Id, ReceiveBlock);
+                }, GenericCanExecute);
+
+            unblock = new DelegateCommand((obj) =>
+                {
+                    IsLoading = true;
+                    ServiceDispatcher.GetService(DataTransfer.CurrentAccount).UnblockUser(user.Id, ReceiveBlock);
+                }, GenericCanExecute);
+
+            reportSpam = new DelegateCommand((obj) =>
+            {
+                IsLoading = true;
+                ServiceDispatcher.GetService(DataTransfer.CurrentAccount).ReportSpam(user.Id, ReceiveReportSpam);
+            }, GenericCanExecute);
+
+            changeAvatar = new DelegateCommand((obj) =>
+                {
+                    PhotoChooserTask task = new PhotoChooserTask();
+                    task.ShowCamera = true;
+                    task.Completed += new EventHandler<PhotoResult>(task_Completed);
+                    task.Show();
+                }, (obj) => GenericCanExecute.Invoke(null) && IsOwner);
+
+            manageLists = new DelegateCommand((obj) => Navigate("/Pages/Lists/ListManager.xaml?user=" + user.ScreenName),
+                GenericCanExecute);
+        }
+
+        public void Loaded(string userName)
+        {
+            Regex remove = new Regex("@|:");
+            userName = remove.Replace(userName, "");
+
+            ScreenName = userName;
+
+            BarText = "Retrieving user...";
+            IsLoading = true;
+            ServiceDispatcher.GetDefaultService().ListUserProfilesFor(new List<string> { userName }, ReceiveUsers);
+        }
+
+        void task_Completed(object sender, PhotoResult e)
+        {
+            UserToken usr;
+            usr = Config.Accounts.FirstOrDefault(item => item != null && item.ScreenName == user.ScreenName);
+            if (e.TaskResult == TaskResult.OK && user != null)
+            {
+                BarText = "Uploading picture...";
+                IsLoading = true;
+                TwitterService srv = ServiceDispatcher.GetService(usr);
+                srv.UpdateProfileImage(e.OriginalFileName, e.ChosenPhoto, ReceivePhotoUpload);
+            }
+        }
+
+        private void ReceivePhotoUpload(TwitterUser user, TwitterResponse response)
+        {
+            BarText = "";
+            IsLoading = false;
+            if (response.StatusCode == HttpStatusCode.OK)
+                MessageService.ShowLightNotification("Your profile image has been changed.");
+            else
+                MessageService.ShowError("An error happened while uploading your image. Verify the file is less than 700 kB of size.");
+        }
+
+        void ReceiveFollow(TwitterUser usr, TwitterResponse response)
+        {
+            string action = Followed ? "unfollow" : "follow";
+            IsLoading = false;
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                MessageService.ShowLightNotification("Now you " + action + " " + usr.ScreenName);
+                Followed = !Followed;
+                followUser.RaiseCanExecuteChanged();
+                unfollowUser.RaiseCanExecuteChanged(); ;
+            }
+            else
+                MessageService.ShowError("We couldn't " + action + " " + user.ScreenName + ".");
+        }
+
+        void ReceiveBlock(TwitterUser usr, TwitterResponse response)
+        {
+            string action = Blocked ? "unblock" : "block";
+            string pastAction = Blocked ? "unblocked" : "blocked";
+            IsLoading = false;
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                MessageService.ShowLightNotification(usr.ScreenName + "is now " + pastAction + ".");
+                Blocked = !Blocked;
+                block.RaiseCanExecuteChanged();
+                unblock.RaiseCanExecuteChanged();
+            }
+            else
+                MessageService.ShowError("We couldn't " + action + " " + user.ScreenName + ".");
+        }
+
+        void ReceiveReportSpam(TwitterUser usr, TwitterResponse response)
+        {
+            IsLoading = false;
+            if (response.StatusCode == HttpStatusCode.OK)
+                MessageService.ShowLightNotification("You have successfully reported and blocked " + usr.ScreenName);
+            else
+                MessageService.ShowError("We couldn't report the user.");
+        }
+
+
+
+        void ReceiveUsers(IEnumerable<TwitterUser> users, TwitterResponse response)
+        {
+            BarText = "";
+            IsLoading = false;
+            if (response.StatusCode != HttpStatusCode.OK || !users.Any())
+            {
+                MessageService.ShowError("We couldn't find the user @" + ScreenName);
+                return;
+            }
+
+            user = users.First();
+
+            Avatar = user.ProfileImageUrl;
+            Name = user.Name;
+            ScreenName = user.ScreenName;
+            Website = user.Url;
+            Biography = user.Description;
+            Tweets = user.StatusesCount.ToString();
+            Followers = user.FollowersCount.ToString();
+            Following = user.FriendsCount.ToString();
+            WebsiteEnabled = Uri.IsWellFormedUriString(user.Url, UriKind.Absolute);
+            IsOwner = Config.Accounts.Any(item => item.Id == user.Id);
+
+            if (DataTransfer.CurrentAccount != null)
+            {
+                var service = ServiceDispatcher.GetService(DataTransfer.CurrentAccount);
+                service.GetFriendshipInfo((int)DataTransfer.CurrentAccount.Id, user.Id, ReceiveFriendshipInfo);
+                service.ListBlockedUserIds(ReceiveBlockedUsers);
+            }
+
+            followUser.RaiseCanExecuteChanged();
+            unfollowUser.RaiseCanExecuteChanged();
+            block.RaiseCanExecuteChanged();
+            unblock.RaiseCanExecuteChanged();
+            pinUser.RaiseCanExecuteChanged();
+            reportSpam.RaiseCanExecuteChanged();
+            manageLists.RaiseCanExecuteChanged();
+            changeAvatar.RaiseCanExecuteChanged();
+        }
+
+        void ReceiveFriendshipInfo(TwitterFriendship friendship, TwitterResponse response)
+        {
+            if (response.StatusCode != HttpStatusCode.OK)
+            {
+                MessageService.ShowWarning("We couldn't get your relationship with this user.");
+                return;
+            }
+
+            Followed = friendship.Relationship.Source.Following;
+
+            followUser.RaiseCanExecuteChanged();
+            unfollowUser.RaiseCanExecuteChanged();
+        }
+
+        void ReceiveBlockedUsers(IEnumerable<int> blockedIds, TwitterResponse response)
+        {
+            Blocked = blockedIds.Any() && blockedIds.Contains(user.Id);
+
+            block.RaiseCanExecuteChanged();
+            unblock.RaiseCanExecuteChanged();
+        }
+    }
+}
