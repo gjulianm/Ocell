fcc67d5c5defafd56e075866e63672e0d4053c03
Guillermo Julián <gjulian93@gmail.com>
2012-05-31 17:08:33 +0200

    Start transition to MVVM in MainPage

--- Ocell/MainPage.xaml.cs
+++ Ocell/MainPage.xaml.cs
@@ -13,6 +13,8 @@ using Ocell.Library.Twitter;
 using TweetSharp;
 using System.Threading;
 using System.Diagnostics;
+using DanielVaughan.Services;
+using DanielVaughan;
 
 
 namespace Ocell
@@ -20,47 +22,24 @@ namespace Ocell
     public partial class MainPage : PhoneApplicationPage
     {
         private ObservableCollection<TwitterResource> pivots;
-        private bool selectionChangeFired;
         private Dictionary<string, ExtendedListBox> Lists;
         private DateTime LastErrorTime;
         private DateTime LastReloadTime;
         private bool _initialised;
-        private Stopwatch _watch;
 
         #region Loaders
         // Constructora
         public MainPage()
         {
-            _watch = new Stopwatch();
             _initialised = false;
             InitializeComponent();
 
             ThemeFunctions.ChangeBackgroundIfLightTheme(LayoutRoot);
 
-            pivots = new ObservableCollection<TwitterResource>();
-            Lists = new Dictionary<string, ExtendedListBox>();
-
             this.Loaded += new RoutedEventHandler(CallLoadFunctions);
-            pivots.CollectionChanged += new System.Collections.Specialized.NotifyCollectionChangedEventHandler(pivots_CollectionChanged);
-            MainPivot.SelectionChanged += new SelectionChangedEventHandler(PivotSelectionChanged);
-
-            MainPivot.DataContext = pivots;
-            MainPivot.ItemsSource = pivots;
 
             LastErrorTime = DateTime.MinValue;
             LastReloadTime = DateTime.MinValue;
-
-            SetUpPivots();
-        }
-
-        private void PivotSelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if (DateTime.Now > LastReloadTime.AddSeconds(25))
-            {
-                LoadTweetsOnPivots();
-                LastReloadTime = DateTime.Now;
-            }
-            RefreshCurrentAccount(e);
         }
 
         void ReloadColumns()
@@ -81,19 +60,14 @@ namespace Ocell
 
         void CallLoadFunctions(object sender, RoutedEventArgs e)
         {
-            if (DataTransfer.ShouldReloadColumns)
-                ReloadColumns();
-
             if (_initialised)
+                return;
+
+            if(!CheckForLogin())
                 return;
 
             ThreadPool.QueueUserWorkItem((threadContext) =>
             {
-                if (Config.RetweetAsMentions == null)
-                    Config.RetweetAsMentions = true;
-                if (Config.TweetsPerRequest == null)
-                    Config.TweetsPerRequest = 40;
-
                 CreateTile();
                 ShowFollowMessage();
                 LittleWatson.CheckForPreviousException();
@@ -101,36 +75,19 @@ namespace Ocell
                     Config.DefaultMuteTime = TimeSpan.FromHours(8);
             });
 
-            string Column;
-            if (NavigationContext.QueryString.TryGetValue("column", out Column))
-                NavigateToColumn(Uri.UnescapeDataString(Column));
-
             _initialised = true;
-        }
-
-        void NavigateToColumn(string Column)
-        {
-            TwitterResource Resource = pivots.FirstOrDefault(item => item.String == Column);
-            if (Resource != null)
-                MainPivot.SelectedItem = Resource;
         }
 
         void ShowFollowMessage()
         {
             if ((Config.FollowMessageShown == false || Config.FollowMessageShown == null) && ServiceDispatcher.CanGetServices)
             {
-                Dispatcher.BeginInvoke(() =>
-                    {
-                        MessageBoxResult Result = MessageBox.Show("Do you want to follow @OcellApp on Twitter to receive the lastest updates?", "", MessageBoxButton.OKCancel);
-                        if (Result == MessageBoxResult.OK)
-                            ServiceDispatcher.GetDefaultService().FollowUser("OcellApp", DummyReceiveFollow);
-                    });
+                var service = Dependency.Resolve<IMessageService>();
+                bool result = service.AskYesNoQuestion("Do you want to follow @OcellApp on Twitter to receive updates?", "");
+                if (result)
+                    ServiceDispatcher.GetDefaultService().FollowUser("OcellApp", (a, b) => { });
                 Config.FollowMessageShown = true;
             }
-        }
-
-        void DummyReceiveFollow(TwitterUser User, TwitterResponse Response)
-        {
         }
 
         void CreateTile()
@@ -140,90 +97,20 @@ namespace Ocell
             TileManager.UpdateTile(null, null); // Updating with null means it will clear the tile.
         }
 
-        void SetUpPivots()
-        {
-            if (Config.Accounts.Count == 0)
-            {
-                ShowLoginMsg();
-                return;
-            }
-
-            DataTransfer.CurrentAccount = Config.Accounts[0];
-            UpdatePivotTitle();
-
-            GetPivotsFromConf();
-        }
-
-
         #endregion
 
-        private void LoadTweetsOnPivots()
+        bool CheckForLogin()
         {
-            if (MainPivot.SelectedItem == null)
-                return;
-            ExtendedListBox ListBox;
-            TwitterResource Resource = (TwitterResource)MainPivot.SelectedItem;
-            if (Lists.TryGetValue(Resource.String, out ListBox))
+            if (!Config.Accounts.Any())
             {
-                Dispatcher.BeginInvoke(() => pBar.IsVisible = true);
-                ListBox.Loader.Load();
-            }
-        }
-
-        private void RefreshCurrentAccount(SelectionChangedEventArgs e)
-        {
-            if (e.AddedItems.Count > 0 && e.AddedItems[0] != null)
-            {
-                TwitterResource item = (TwitterResource)e.AddedItems[0];
-                DataTransfer.CurrentAccount = item.User;
-                UpdatePivotTitle();
-            }
-        }
-        void BindPivots()
-        {
-            MainPivot.DataContext = null;
-            MainPivot.ItemsSource = pivots;
-        }
-
-        void ShowLoginMsg()
-        {
-            Config.TweetsPerRequest = 40;
-            Dispatcher.BeginInvoke(() =>
-            {
-                MessageBoxResult r = MessageBox.Show("You have to log in with Twitter in order to use Ocell.", "", MessageBoxButton.OKCancel);
-                if (r == MessageBoxResult.OK)
+                var service = Dependency.Resolve<IMessageService>();
+                bool result = service.AskYesNoQuestion("You have to log in with Twitter in order to use Ocell.", "");
+                if (result)
                     NavigationService.Navigate(Uris.LoginPage);
-            });
-        }
-
-        void GetPivotsFromConf()
-        {
-            foreach (var pivot in Config.Columns)
-                if (!pivots.Contains(pivot))
-                    pivots.Add(pivot);
-        }
-
-        void UpdatePivotTitle()
-        {
-            Dispatcher.BeginInvoke(() =>
-            {
-                MainPivot.Title = "OCELL";
-                if (DataTransfer.CurrentAccount != null && !string.IsNullOrWhiteSpace(DataTransfer.CurrentAccount.ScreenName))
-                    MainPivot.Title += " - " + DataTransfer.CurrentAccount.ScreenName.ToUpperInvariant();
-            });
-        }
-
-        void pivots_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
-        {
-            BindPivots();
-        }
-
-        private void compose_Click(object sender, System.EventArgs e)
-        {
-            if (Config.Accounts.Count == 0)
-                ShowLoginMsg();
+                return false;
+            }
             else
-                NavigationService.Navigate(Uris.WriteTweet);
+                return true;
         }
 
         private void ListBox_Loaded(object sender, RoutedEventArgs e)
@@ -234,8 +121,8 @@ namespace Ocell
             var tag = list.Tag;
 
             ThreadPool.QueueUserWorkItem((threadcontext) =>
-            {                
-                TwitterResource Resource = new TwitterResource();             
+            {
+                TwitterResource Resource = new TwitterResource();
 
                 if (tag is TwitterResource)
                 {
@@ -319,46 +206,10 @@ namespace Ocell
             NavigationService.Navigate(Uris.Settings);
         }
 
-        private void add_Click(object sender, EventArgs e)
-        {
-            if (Config.Accounts.Count == 0)
-                ShowLoginMsg();
-            else
-                NavigationService.Navigate(Uris.Columns);
-        }
-
         private void MainPivot_Loaded(object sender, RoutedEventArgs e)
         {
             MainPivot.DataContext = pivots;
             MainPivot.ItemsSource = pivots;
-        }
-
-        private void send_DM_Click(object sender, System.EventArgs e)
-        {
-            NavigationService.Navigate(Uris.SelectUserForDM);
-        }
-
-        private void SearchBtn_Click(object sender, System.EventArgs e)
-        {
-            NavigationService.Navigate(Uris.SearchForm);
-        }
-
-        private void pinToStart_Click(object sender, System.EventArgs e)
-        {
-            if (SecondaryTiles.ColumnTileIsCreated((TwitterResource)MainPivot.SelectedItem))
-                Dispatcher.BeginInvoke(() => MessageBox.Show("This column is already pinned."));
-            else
-                SecondaryTiles.CreateColumnTile((TwitterResource)MainPivot.SelectedItem);
-        }
-
-        private void about_Click(object sender, System.EventArgs e)
-        {
-            NavigationService.Navigate(Uris.About);
-        }
-
-        private void Trending_Click(object sender, System.EventArgs e)
-        {
-            NavigationService.Navigate(Uris.TrendingTopics);
         }
 
         private void ApplicationBarMenuItem_Click(object sender, System.EventArgs e)
---
+++ Ocell/MainPageModel.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Data;
+using System.Windows.Input;
+using DanielVaughan.ComponentModel;
+using DanielVaughan.Windows;
+using TweetSharp;
+using Ocell.Library;
+using Ocell.Library.Twitter;
+using System.Collections.Generic;
+using System;
+using System.ComponentModel;
+using System.Threading;
+using DanielVaughan;
+using DanielVaughan.InversionOfControl;
+using DanielVaughan.Net;
+using DanielVaughan.Services;
+using System.Linq;
+
+namespace Ocell.Pages
+{
+    public class ReloadArgs : EventArgs
+    {
+        public TwitterResource Resource { get; set; }
+        public bool ReloadAll { get; set; }
+
+        public ReloadArgs(TwitterResource resource, bool reloadAll = false)
+        {
+            Resource = resource;
+            ReloadAll = reloadAll;
+        }
+    }
+
+    public delegate void ReloadEventHandler(object sender, ReloadArgs e);
+
+    public class AddColumnModel : ViewModelBase
+    {
+        DateTime lastAutoReload;
+        const int secondsBetweenReloads = 25;
+
+        public event ReloadEventHandler ReloadLists;
+        private void RaiseReload(TwitterResource resource)
+        {
+            var temp = ReloadLists;
+            if (temp != null)
+                temp(this, new ReloadArgs(resource, false));
+        }
+
+        private void RaiseReloadAll()
+        {
+            var temp = ReloadLists;
+            if (temp != null)
+                temp(this, new ReloadArgs(Config.Columns.FirstOrDefault(), true));
+        }
+
+        bool isLoading;
+        public bool IsLoading
+        {
+            get { return isLoading; }
+            set { Assign("IsLoading", ref isLoading, value); }
+        }
+
+        SafeObservable<TwitterResource> pivots;
+        public SafeObservable<TwitterResource> Pivots
+        {
+            get { return pivots; }
+            set { Assign("Pivots", ref pivots, value); }
+        }
+
+        object selectedPivot;
+        public object SelectedPivot
+        {
+            get { return selectedPivot; }
+            set { Assign("SelectedPivot", ref selectedPivot, value); }
+        }
+
+        string currentAccountName;
+        public string CurrentAccountName
+        {
+            get { return currentAccountName; }
+            set { Assign("CurrentAccountName", ref currentAccountName, value); }
+        }
+
+        public AddColumnModel()
+            : base("AddColumn")
+        {
+            if (Config.RetweetAsMentions == null)
+                Config.RetweetAsMentions = true;
+            if (Config.TweetsPerRequest == null)
+                Config.TweetsPerRequest = 40;
+            if (Config.DefaultMuteTime == null || Config.DefaultMuteTime == TimeSpan.FromHours(0))
+                Config.DefaultMuteTime = TimeSpan.FromHours(8);
+
+            lastAutoReload = DateTime.MinValue;
+            Pivots = new SafeObservable<TwitterResource>();
+
+            foreach (var pivot in Config.Columns)
+                Pivots.Add(pivot);
+
+            Config.Columns.CollectionChanged += (sender, e) =>
+            {
+                foreach (var item in e.NewItems)
+                {
+                    if ((item is TwitterResource) && !Pivots.Contains((TwitterResource)item))
+                        Pivots.Add((TwitterResource)item);
+                }
+
+                foreach (var item in e.OldItems)
+                {
+                    if ((item is TwitterResource) && Pivots.Contains((TwitterResource)item))
+                        Pivots.Remove((TwitterResource)item);
+                }
+            };
+
+            this.PropertyChanged += (sender, e) =>
+                {
+                    if (e.PropertyName == "SelectedPivot")
+                        UpdatePivot();
+                };
+
+            this.NavigatedTo += (sender, e) => ThreadPool.QueueUserWorkItem((context) => RaiseReloadAll());
+
+            string column;
+            if (QueryParameters.TryGetValue("column", out column))
+            {
+                column = Uri.UnescapeDataString(column);
+                if(Config.Columns.Any(item => item.String == column))
+                    SelectedPivot = Config.Columns.First(item => item.String == column);
+            }
+        }
+
+        void UpdatePivot()
+        {
+            if (SelectedPivot is TwitterResource)
+            {
+                var resource = (TwitterResource)SelectedPivot;
+                CurrentAccountName = resource.User.ScreenName;
+                if (DateTime.Now > lastAutoReload.AddSeconds(secondsBetweenReloads))
+                {
+                    lastAutoReload = DateTime.Now;
+                    ThreadPool.QueueUserWorkItem((context) => RaiseReload(resource));
+                }
+                DataTransfer.CurrentAccount = resource.User;
+            }
+        }
+    }
+}
