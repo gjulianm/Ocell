5dbcbda9c57cc6b8de6c088025555a01774451ff
Guillermo Julián <gjulian93@gmail.com>
2012-05-31 17:08:55 +0200

    Some changes.

--- Ocell/Pages/Elements/Conversation.xaml.cs
+++ Ocell/Pages/Elements/Conversation.xaml.cs
@@ -17,16 +17,13 @@ namespace Ocell.Pages.Elements
         private TwitterStatus LastStatus;
         private ObservableCollection<TwitterStatus> Source;
         private bool selectionChangeFired = false;
-        private ConversationService replies;
-        private int pendingCalls;
+
         public Conversation()
         {
             InitializeComponent(); ThemeFunctions.ChangeBackgroundIfLightTheme(LayoutRoot);
             
             this.Loaded += new RoutedEventHandler(Conversation_Loaded);
-            replies = new ConversationService(DataTransfer.CurrentAccount);
             Source = new ObservableCollection<TwitterStatus>();
-            pendingCalls = 0;
         }
         
         private void Conversation_Loaded(object sender, RoutedEventArgs e)
@@ -36,16 +33,6 @@ namespace Ocell.Pages.Elements
                 NavigationService.GoBack();
                 return;
             }
-
-            /*if (!Source.Contains(DataTransfer.Status))
-                Source.Clear();
-
-            LastStatus = DataTransfer.Status;
-            if(!Source.Contains(DataTransfer.Status))
-            	Source.Add(LastStatus);
-            CList.ItemsSource = Source;
-
-            MoreConversation();*/
 
             TwitterResource resource = new TwitterResource
             {
@@ -64,43 +51,11 @@ namespace Ocell.Pages.Elements
                 CList.Loader.Source.Clear();
                 CList.Loader.Resource = resource;
             }
-            
-            Dispatcher.BeginInvoke(() => pBar.IsVisible = true);
-            CList.Loader.LoadFinished += new EventHandler(LoadFinished);
+
+            DataContext = CList.Loader;
+
             CList.Loader.Cached = false;
-            CList.Loader.Load();
-            
-        }
-
-        private void LoadFinished(object sender, EventArgs e)
-        {
-            Dispatcher.BeginInvoke(() => pBar.IsVisible = false);
-        }
-        
-        private void ListBox_Loaded(object sender, RoutedEventArgs e)
-        {
-            
-        }
-    
-        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if (!selectionChangeFired)
-            {
-                DataTransfer.Status = e.AddedItems[0] as TwitterStatus;
-                ListBox list = sender as ListBox;
-                selectionChangeFired = true;
-                list.SelectedIndex = -1;
-
-                NavigationService.Navigate(Uris.ViewTweet);
-            }
-            else
-                selectionChangeFired = false;
-        }
-        private void EndLoad()
-        {
-            pendingCalls--;
-            if(pendingCalls <= 0)
-                Dispatcher.BeginInvoke(() => pBar.IsVisible = false);
+            CList.Loader.Load();            
         }
     }
 }
