fdb9127b992deedbabe0fc25bae174253af99557
Guillermo Julián <gjulian93@gmail.com>
2012-05-31 23:11:01 +0200

    Tweaks for testing.
    When testing, SystemTray.ProgressIndicator throws an exception. Just
    catch and do nothing.

--- Library/MessageServiceExtension.cs
+++ Library/MessageServiceExtension.cs
@@ -22,25 +22,40 @@ namespace Ocell.Library
 
             dispatcher.BeginInvoke(() =>
                 {
-                    var bar = SystemTray.ProgressIndicator;
-                    if (bar == null)
-                        return;
-                    bar.IsIndeterminate = false;
-                    bar.Value = 0;
-                    bar.Text = message;
-                    bar.IsVisible = true;
+                    try
+                    {
+                        var bar = SystemTray.ProgressIndicator;
+                        if (bar == null)
+                            return;
+                        bar.IsIndeterminate = false;
+                        bar.Value = 0;
+                        bar.Text = message;
+                        bar.IsVisible = true;
+                    }
+                    catch (Exception)
+                    {
+                        // On testing, Exceptions are thrown. So, if testing, don't do anything.
+                    }
                 });
 
             var timer = new Timer((context) =>
                 {
                     dispatcher.BeginInvoke(() =>
                         {
-                            var bar = SystemTray.ProgressIndicator;
-                            if (bar == null)
-                                return;
-                            bar.IsVisible = false;
-                            bar.IsIndeterminate = true;
-                            bar.Text = "";
+                            try
+                            {
+
+                                var bar = SystemTray.ProgressIndicator;
+                                if (bar == null)
+                                    return;
+                                bar.IsVisible = false;
+                                bar.IsIndeterminate = true;
+                                bar.Text = "";
+                            }
+                            catch (Exception)
+                            {
+                                // same as above.
+                            }
                         });
                 }, null, 3500, Timeout.Infinite);
 
