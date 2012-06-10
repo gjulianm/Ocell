aeccfc5cc4443bb527070ca59bf903a478d0254c
Guillermo Julián <gjulian93@gmail.com>
2012-06-10 12:13:26 +0200

    Removed unused namespace references.

--- Library/DateSync.cs
+++ Library/DateSync.cs
@@ -1,18 +1,7 @@
 ﻿using System;
-using System.Net;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Ink;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Shapes;
 using System.IO.IsolatedStorage;
-using System.Text;
-using Microsoft.Phone.Scheduler;
-using Microsoft.Phone.Shell;
 using System.Threading;
+
 namespace Ocell.Library
 {
     public static class DateSync
