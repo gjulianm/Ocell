6aca3fa69351880931deb37b1d7c759a08a4c7c8
Guillermo Julián <gjulian93@gmail.com>
2012-07-13 17:41:20 +0200

    Added capability to view followers and followings.

---
+++ Ocell/Pages/Elements/UserList.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using Ocell.Pages.Elements;
+
+namespace Ocell
+{
+    public partial class UserList : PhoneApplicationPage
+    {
+        UserListModel viewModel;
+
+        public UserList()
+        {
+            InitializeComponent();
+            viewModel = new UserListModel();
+            DataContext = viewModel;
+
+            ThemeFunctions.SetBackground(LayoutRoot);
+            this.Loaded += (sender, e) =>
+            {
+                string resource, user;
+                if (!NavigationContext.QueryString.TryGetValue("resource", out resource) || !NavigationContext.QueryString.TryGetValue("user", out user))
+                {
+                    Dispatcher.BeginInvoke(() => {
+                        MessageBox.Show("An error has occurred.", "Error", MessageBoxButton.OK);
+                        NavigationService.GoBack();
+                    });
+                    
+                    return;
+                }
+
+                viewModel.Loaded(resource, user);
+            };
+        }
+    }
+}
---
+++ Ocell/Pages/Elements/UserListModel.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Windows.Input;
+using DanielVaughan.ComponentModel;
+using DanielVaughan.Windows;
+using Microsoft.Phone.Tasks;
+using Ocell.Library;
+using Ocell.Library.Twitter;
+using TweetSharp;
+using System.Windows.Data;
+
+namespace Ocell.Pages.Elements
+{
+    public class UserListModel : ExtendedViewModelBase
+    {
+        string whatUserList;
+        string user;
+
+        bool isLoading;
+        public bool IsLoading
+        {
+            get { return isLoading; }
+            set { Assign("IsLoading", ref isLoading, value); }
+        }
+
+        string barText;
+        public string BarText
+        {
+            get { return barText; }
+            set { Assign("BarText", ref barText, value); }
+        }
+
+        string pageTitle;
+        public string PageTitle
+        {
+            get { return pageTitle; }
+            set { Assign("PageTitle", ref pageTitle, value); }
+        }
+
+        SafeObservable<TwitterUser> list;
+
+        CollectionViewSource viewSource;
+
+        public System.ComponentModel.ICollectionView List
+        {
+            get { return viewSource.View; }
+        }
+
+        object selectedUser;
+        public object SelectedUser
+        {
+            get { return selectedUser; }
+            set { Assign("SelectedUser", ref selectedUser, value); }
+        }
+
+        public UserListModel()
+            : base("UserList")
+        {
+            whatUserList ="";
+            user = "";
+            PageTitle = whatUserList;
+            list = new SafeObservable<TwitterUser>();
+            viewSource = new CollectionViewSource();
+            viewSource.Source = list;
+            viewSource.View.SortDescriptions.Add(new System.ComponentModel.SortDescription("ScreenName", System.ComponentModel.ListSortDirection.Ascending));
+
+
+            
+
+            
+
+            this.PropertyChanged += (sender, e) =>
+            {
+                if (e.PropertyName == "SelectedUser")
+                {
+                    TwitterUser selected = SelectedUser as TwitterUser;
+                    if (selected != null)
+                    {                        
+                        Navigate("/Pages/Elements/User.xaml?user=" + selected.ScreenName);
+                        SelectedUser = null;
+                    }
+                }
+            };
+        }
+
+        public void Loaded(string resource, string userName)
+        {
+            whatUserList = resource;
+            user = userName;
+
+            if (whatUserList == "followers")
+                ServiceDispatcher.GetCurrentService().ListFollowersOf(user, ReceiveUsers);
+            else if (whatUserList == "following")
+                ServiceDispatcher.GetCurrentService().ListFriendsOf(user, ReceiveUsers);
+            else
+            {
+                MessageService.ShowError("That's not a valid resource.");
+                GoBack();
+                return;
+            }
+
+            PageTitle = whatUserList;
+
+            IsLoading = true;
+            BarText = "downloading " + whatUserList + "...";
+        }
+
+
+        private void ReceiveUsers(TwitterCursorList<TwitterUser> users, TwitterResponse response)
+        {
+            if (response.StatusCode == HttpStatusCode.NotFound)
+            {
+                MessageService.ShowError("We couldn't find that user");
+                GoBack();
+                return;
+            }
+            else if (response.StatusCode != HttpStatusCode.OK)
+            {
+                MessageService.ShowError("An error has occurred.");
+                GoBack();
+                return;
+            }
+
+            foreach (var usr in users)
+                if (!list.Contains(usr))
+                    list.Add(usr);
+
+            if (users.NextCursor != null && users.NextCursor != 0)
+            {
+                if (whatUserList == "followers")
+                    ServiceDispatcher.GetCurrentService().ListFollowersOf(user, ReceiveUsers);
+                else if (whatUserList == "following")
+                    ServiceDispatcher.GetCurrentService().ListFriendsOf(user, ReceiveUsers);
+            }
+            else
+            {
+                IsLoading = false;
+            }
+        }
+    }
+}
