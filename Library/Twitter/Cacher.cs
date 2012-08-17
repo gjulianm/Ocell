e38989da1563e75f223356d55cb7e060547039a5
Guillermo Julián <gjulian93@gmail.com>
2012-08-17 13:55:45 +0200

    Revert "Revert "Save user list on a local file for faster access. Also, bug fixes.""
    This reverts commit cb47521ef01d7d6e2099af4cd43e86abad5ad872.

--- Library/Twitter/Cacher.cs
+++ Library/Twitter/Cacher.cs
@@ -85,7 +85,7 @@ namespace Ocell.Library.Twitter
             string Key = GetCacheName(Resource);
             List<string> Strings = new List<string>();
 
-			List = List.Distinct(new TwitterStatusEqualityComparer());
+			List = List.Distinct();
 						
             try
             {
