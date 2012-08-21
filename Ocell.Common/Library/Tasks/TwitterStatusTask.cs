a08f1c49da1e96c33796d98a75846ce24dca9225
Guillermo Julián Moreno <gjulian93@gmail.com>
2012-08-21 23:48:15 +0200

    New directory structure
    Ready to hold upcoming Windows 8 Ocell version, maximizing code
    reusability.

---
+++ Ocell.Common/Library/Tasks/TwitterStatusTask.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using Ocell.Library.Twitter;
+using TweetSharp;
+
+namespace Ocell.Library.Tasks
+{
+    public class TwitterStatusTask
+    {
+        public string Text { get; set; }
+        public long InReplyTo { get; set; }
+        public IList<UserToken> Accounts { get; set; }
+        public DateTime Scheduled { get; set; }
+    }
+}
