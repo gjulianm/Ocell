bde8435aabb5757fa76435aee2ca1a4c0c0774f7
Guillermo <gjulian93@gmail.com>
2012-08-26 18:07:49 +0200

    Created project structure

---
+++ Ocell.Windows8/Ocell.Windows8/App.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla Aplicación vacía está documentada en http://go.microsoft.com/fwlink/?LinkId=234227
+
+namespace Ocell.Windows8
+{
+    /// <summary>
+    /// Proporciona un comportamiento específico de la aplicación para complementar la clase Application predeterminada.
+    /// </summary>
+    sealed partial class App : Application
+    {
+        /// <summary>
+        /// Inicializa el objeto de aplicación Singleton. Esta es la primera línea de código creado
+        /// ejecutado y, como tal, es el equivalente lógico de main() o WinMain().
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+            this.Suspending += OnSuspending;
+        }
+
+        /// <summary>
+        /// Se invoca cuando la aplicación la inicia normalmente el usuario final. Se usarán otros puntos
+        /// de entrada cuando la aplicación se inicie para abrir un archivo específico, para mostrar
+        /// resultados de la búsqueda, etc.
+        /// </summary>
+        /// <param name="args">Información detallada acerca de la solicitud y el proceso de inicio.</param>
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
+        {
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            // No repetir la inicialización de la aplicación si la ventana tiene contenido todavía,
+            // solo asegurarse de que la ventana está activa.
+            if (rootFrame == null)
+            {
+                // Crear un marco para que actúe como contexto de navegación y navegar a la primera página.
+                rootFrame = new Frame();
+
+                if (args.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                    //TODO: Cargar el estado de la aplicación suspendida previamente
+                }
+
+                // Poner el marco en la ventana actual.
+                Window.Current.Content = rootFrame;
+            }
+
+            if (rootFrame.Content == null)
+            {
+                // Cuando no se restaura la pila de navegación para navegar a la primera página,
+                // configurar la nueva página al pasar la información requerida como parámetro
+                // parámetro
+                if (!rootFrame.Navigate(typeof(MainPage), args.Arguments))
+                {
+                    throw new Exception("Failed to create initial page");
+                }
+            }
+            // Asegurarse de que la ventana actual está activa.
+            Window.Current.Activate();
+        }
+
+        /// <summary>
+        /// Se invoca al suspender la ejecución de la aplicación. El estado de la aplicación se guarda
+        /// sin saber si la aplicación se terminará o se reanudará con el contenido
+        /// de la memoria aún intacto.
+        /// </summary>
+        /// <param name="sender">Origen de la solicitud de suspensión.</param>
+        /// <param name="e">Detalles sobre la solicitud de suspensión.</param>
+        private void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            //TODO: Guardar el estado de la aplicación y detener toda actividad en segundo plano
+            deferral.Complete();
+        }
+    }
+}
