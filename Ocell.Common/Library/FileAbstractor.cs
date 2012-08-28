d4b440e6fdc8cedf40d772e298d1cb1b977b6634
Guillermo Julián Moreno <gjulian93@gmail.com>
2012-08-28 12:18:07 +0200

    FileAbstractor now works syncronously.
    Maybe later I add async methods for W8, but this improves compatibility.

--- Ocell.Common/Library/FileAbstractor.cs
+++ Ocell.Common/Library/FileAbstractor.cs
@@ -15,11 +15,8 @@ namespace Ocell.Library
     public static class FileAbstractor
     {
 
-#if METRO
-        public static async void WriteContentsToFile(string contents, string fileName)
-#else
-        public static void WriteContentsToFile(string contents, string file)
-#endif
+
+        public static void WriteContentsToFile(string contents, string fileName)
         {
             Mutex mutex = new Mutex(false, "OCELL_FILE_MUTEX" + fileName);
 #if METRO
@@ -28,8 +25,10 @@ namespace Ocell.Library
             {
                 try
                 {
-                    var file = await storage.CreateFileAsync(fileName, CreationCollisionOption.ReplaceExisting);
-                    await FileIO.WriteTextAsync(file, contents);
+                    var fileTask = storage.CreateFileAsync(fileName, CreationCollisionOption.ReplaceExisting).AsTask();
+                    fileTask.Wait();
+                    var file = fileTask.Result;
+                    FileIO.WriteTextAsync(file, contents).AsTask().RunSynchronously();
                 }
                 catch (Exception)
                 {
@@ -67,17 +66,10 @@ namespace Ocell.Library
 #endif
         }
 
-#if METRO
-        public static async Task<string> ReadContentsOfFile(string fileName)
-#else
+
         public static string ReadContentsOfFile(string fileName)
-#endif
         {
-#if METRO
-            var lines = await ReadLinesOfFile(fileName);
-#else
             var lines = ReadLinesOfFile(fileName);
-#endif
             return lines.Aggregate("", (accumulate, x) => accumulate + x + '\n');
         }
 
@@ -88,11 +80,8 @@ namespace Ocell.Library
             WriteContentsToFile(contents, fileName);
         }
 
-#if METRO
-        public static async Task<IEnumerable<string>> ReadLinesOfFile(string fileName)
-#else
-        public static string IEnumerable<string> ReadLinesOfFile(string fileName)
-#endif
+
+        public static IEnumerable<string> ReadLinesOfFile(string fileName)
         {
             Mutex mutex = new Mutex(false, "OCELL_FILE_MUTEX" + fileName);
             IEnumerable<string> contents = new List<string>();
@@ -104,8 +93,12 @@ namespace Ocell.Library
             {
                 try
                 {
-                    var file = await storage.CreateFileAsync(fileName, CreationCollisionOption.OpenIfExists);
-                    contents = await FileIO.ReadLinesAsync(file);
+                    var fileTask = storage.CreateFileAsync(fileName, CreationCollisionOption.OpenIfExists).AsTask();
+                    fileTask.Wait();
+                    var file = fileTask.Result;
+                    var readTask = FileIO.ReadLinesAsync(file).AsTask();
+                    readTask.Wait();
+                    contents = readTask.Result;
                 }
                 catch (Exception)
                 {
@@ -120,6 +113,48 @@ namespace Ocell.Library
             return contents;
 #else
 #endif
+        }
+
+        /// <summary>
+        /// Read a series of blocks from a file, return them in a list. Block = string with whatever char in them, including newlines.
+        /// </summary>
+        /// <param name="fileName">File name</param>
+        /// <returns>List of blocks.</returns>
+        public static IEnumerable<string> ReadBlocksOfFile(string fileName)
+        {
+            char separator = char.MaxValue;
+            List<string> Strings = new List<string>();
+
+            string contents = ReadContentsOfFile(fileName);
+            string line = "";
+            int newlineIndex;
+
+            while ((newlineIndex = contents.IndexOf(separator)) != -1)
+            {
+                line = contents.Substring(0, newlineIndex);
+                contents = contents.Substring(newlineIndex + 1);
+                Strings.Add(line);
+            }
+
+            return Strings;
+        }
+
+        /// <summary>
+        /// Writes a series of blocks to a file. A block can contain any character (including newlines),
+        /// and it will still be returned without problems.
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="blocks"></param>
+        public static void WriteBlocksToFile(IEnumerable<string> blocks, string fileName)
+        {
+            char separator = char.MaxValue;
+
+            string contents = "";
+
+            foreach (var str in blocks)
+                contents += str + separator;
+
+            WriteContentsToFile(contents, fileName);
         }
 
     }
