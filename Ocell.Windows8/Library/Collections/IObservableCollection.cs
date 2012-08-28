ed866035e2bee51042bc85da9387140e6e3b1851
Guillermo <gjulian93@gmail.com>
2012-08-28 20:40:40 +0200

    Created custom CollectionView

---
+++ Ocell.Windows8/Library/Collections/IObservableCollection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.Specialized;
+using System.Threading.Tasks;
+
+namespace Ocell.Library.Collections
+{
+    public interface IObservableCollection<T> : IEnumerable<T>, INotifyCollectionChanged
+    {
+    }
+}
---
+++ Ocell.Windows8/Library/Collections/SortDescription.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ocell.Library.Collections
+{
+    public enum SortDirection { Ascending, Descending }
+
+    public interface ISortDescription<T>
+    {
+        IEnumerable<T> Sort(IEnumerable<T> collection);
+    }
+
+    public class SortDescription<T, TKey> : ISortDescription<T>
+    {
+        public Func<T, TKey> KeySelector { get; set; }
+
+        public IComparer<TKey> Comparer { get; set; }
+
+        public SortDirection Direction { get; set; }
+
+        public IEnumerable<T> Sort(IEnumerable<T> collection)
+        {
+            if (KeySelector == null)
+                return collection;
+
+            if (Comparer == null)
+            {
+                if (Direction == SortDirection.Ascending)
+                    return collection.OrderBy(KeySelector);
+                else
+                    return collection.OrderByDescending(KeySelector);
+            }
+            else
+            {
+                if (Direction == SortDirection.Ascending)
+                    return collection.OrderBy(KeySelector, Comparer);
+                else
+                    return collection.OrderByDescending(KeySelector, Comparer);
+            }
+        }
+    }
+}
