55f23cf8da298638ae62b9b387eb955c4ad12a79
Guillermo <gjulian93@gmail.com>
2012-08-28 20:39:39 +0200

    Forgot two using

--- Ocell.Common/Library/DebugWriter.cs
+++ Ocell.Common/Library/DebugWriter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 
 namespace Ocell.Library
 {
--- Ocell.Common/Library/Twitter/ConversationService.cs
+++ Ocell.Common/Library/Twitter/ConversationService.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Net;
 using TweetSharp;
+using System.Linq;
 
 namespace Ocell.Library.Twitter
 {
