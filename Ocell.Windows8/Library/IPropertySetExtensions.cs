6524d0d40ac288ad437b6a93311e49420d5df80c
Guillermo Julián Moreno <gjulian93@gmail.com>
2012-08-28 12:19:15 +0200

    Some compatibility extensions in IPropertySet

---
+++ Ocell.Windows8/Library/IPropertySetExtensions.cs
@@ -0,0 +1,36 @@
+﻿#if METRO
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Foundation.Collections;
+namespace Ocell.Library
+{
+    public static class IPropertySetExtensions
+    {
+        public static bool TryGetValue<T>(this IPropertySet set, string key, out T item)
+        {
+            item = default(T);
+
+            if (!set.ContainsKey(key))
+                return false;
+
+            object obj = set[key];
+
+            if (!(obj is T))
+                return false;
+
+            item = (T)obj;
+
+            return true;
+        }
+
+        public static void Save(this IPropertySet set)
+        {
+            // Do nothing. Just a compatibility function.
+        }
+    }
+}
+#endif
