1b631170f92c8f0d42fe7030af8e9d62e09400ba
Guillermo Julián Moreno <gjulian93@gmail.com>
2012-09-25 00:39:22 +0200

    Migrated AddColumn to MVVM, included list suscriptions.
    I only loaded lists created by the users. Now it loads both lists
    created by and lists the user follows.

--- Ocell.WindowsPhone/Ocell/Pages/Columns/AddColumn.xaml.cs
+++ Ocell.WindowsPhone/Ocell/Pages/Columns/AddColumn.xaml.cs
@@ -15,8 +15,8 @@ namespace Ocell.Pages.Columns
 {
     public partial class AddColumn : PhoneApplicationPage
     {
-        public ObservableCollection<TwitterList> lists;
-        private ITwitterService _srv;
+        AddColumnModel viewModel;
+
         public AddColumn()
         {
             InitializeComponent(); Loaded += (sender, e) => { if (ApplicationBar != null) ApplicationBar.MatchOverriddenTheme(); };
@@ -25,73 +25,15 @@ namespace Ocell.Pages.Columns
                 ApplicationBar.MatchOverriddenTheme(); 
             ThemeFunctions.SetBackground(LayoutRoot);
 
-            this.Loaded += new RoutedEventHandler(AddColumn_Loaded);
-           
+            viewModel = new AddColumnModel();
+            DataContext = viewModel;
+            this.Loaded += new RoutedEventHandler(AddColumn_Loaded);           
         }
 
         void AddColumn_Loaded(object sender, RoutedEventArgs e)
         {
             Dispatcher.BeginInvoke(() => { NavigationService.RemoveBackEntry(); });
-            LoadLists();
-        }
-
-        private void CoreList_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
-        {
-            TwitterResource toAdd = new TwitterResource(); 
-            ListBoxItem item = e.AddedItems[0] as ListBoxItem;
-
-            toAdd.String = DataTransfer.CurrentAccount.ScreenName +";" + (item.Tag as string);
-            toAdd.User = DataTransfer.CurrentAccount;
-            SaveColumn(toAdd);
-            DataTransfer.ShouldReloadColumns = true;
-            NavigationService.GoBack();
-        }
-
-        private void ListsList_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
-        {
-            TwitterResource toAdd;
-            TwitterList item = e.AddedItems[0] as TwitterList;
-
-            toAdd = new TwitterResource { Type = ResourceType.List, Data = item.FullName, User = DataTransfer.CurrentAccount };
-            SaveColumn(toAdd);
-            DataTransfer.ShouldReloadColumns = true;
-            NavigationService.GoBack();
-        }
-
-        private void SaveColumn(TwitterResource Resource)
-        {
-            if (!Config.Columns.Contains(Resource))
-            {
-                Config.Columns.Add(Resource);
-                Config.SaveColumns();
-            }
-        }
-
-        private void ApplicationBarIconButton_Click(object sender, EventArgs e)
-        {
-            LoadLists();
-        }
-
-        private void LoadLists()
-        {
-            _srv = ServiceDispatcher.GetCurrentService();
-            if (_srv != null)
-            {
-                Dispatcher.BeginInvoke(() => { pBar.IsVisible = true; });
-                _srv.ListListsFor(DataTransfer.CurrentAccount.ScreenName, -1, (tlist, resp) =>
-                {
-                    if (resp.StatusCode == HttpStatusCode.OK)
-                    {
-                        lists = new ObservableCollection<TwitterList>(tlist);
-                        Dispatcher.BeginInvoke(() =>
-                        {
-                            ListsList.DataContext = lists;
-                            ListsList.ItemsSource = lists;
-                            pBar.IsVisible = false;
-                        });
-                    }
-                });
-            }
+            viewModel.OnLoad();
         }
     }
 }
---
+++ Ocell.WindowsPhone/Ocell/Pages/Columns/AddColumnModel.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using TweetSharp;
+using Ocell.Library.Twitter;
+using Ocell.Library;
+using System.Collections.Generic;
+using System.Threading;
+using DanielVaughan.Windows;
+using System.Linq;
+
+namespace Ocell.Pages.Columns
+{
+    public class AddColumnModel : ExtendedViewModelBase
+    {
+        SafeObservable<TwitterList> lists;
+        public SafeObservable<TwitterList> Lists
+        {
+            get { return lists; }
+            set { Assign("Lists", ref lists, value); }
+        }
+
+        ObservableCollection<TwitterResource> core;
+        public ObservableCollection<TwitterResource> Core
+        {
+            get { return core; }
+            set { Assign("Core", ref core, value); }
+        }
+
+        object coreSelection;
+        public object CoreSelection
+        {
+            get { return coreSelection; }
+            set { Assign("CoreSelection", ref coreSelection, value); }
+        }
+
+        object listSelection;
+        public object ListSelection
+        {
+            get { return listSelection; }
+            set { Assign("ListSelection", ref listSelection, value); }
+        }
+
+        DelegateCommand reloadLists;
+        public ICommand ReloadLists
+        {
+            get { return reloadLists; }
+        }
+
+        public AddColumnModel()
+            : base("AddColumn")
+        {
+            Core = new ObservableCollection<TwitterResource>();
+            Lists = new SafeObservable<TwitterList>();
+
+            reloadLists = new DelegateCommand((param) => LoadLists(), (param) => !IsLoading);
+
+            this.PropertyChanged += (sender, e) =>
+            {
+                if (e.PropertyName == "ListSelection")
+                    AddSelectedList();
+                else if (e.PropertyName == "CoreSelection")
+                    AddSelectedCoreResource();
+            };
+        }
+
+        int loading = 0;
+        object sync = new object();
+
+        public void OnLoad()
+        {
+            if (DataTransfer.CurrentAccount == null)
+            {
+                MessageService.ShowError(Localization.Resources.ErrorNoAccount);
+                GoBack();
+                return;
+            }
+
+            CreateCoreList();
+
+            LoadLists();
+        }
+
+        private void LoadLists()
+        {
+            var service = ServiceDispatcher.GetService(DataTransfer.CurrentAccount);
+
+            IsLoading = true;
+            BarText = Localization.Resources.LoadingLists;
+
+            loading = 2;
+            service.ListListsFor(DataTransfer.CurrentAccount.ScreenName, -1, ReceiveLists);
+            service.ListListSubscriptionsFor(DataTransfer.CurrentAccount.ScreenName, ReceiveLists);
+        }
+
+        private void CreateCoreList()
+        {
+            Core.Add(new TwitterResource
+            {
+                User = DataTransfer.CurrentAccount,
+                Type = ResourceType.Home
+            });
+
+            Core.Add(new TwitterResource
+            {
+                User = DataTransfer.CurrentAccount,
+                Type = ResourceType.Mentions
+            });
+
+            Core.Add(new TwitterResource
+            {
+                User = DataTransfer.CurrentAccount,
+                Type = ResourceType.Messages
+            });
+
+            Core.Add(new TwitterResource
+            {
+                User = DataTransfer.CurrentAccount,
+                Type = ResourceType.Favorites
+            });
+        }
+
+        private void ReceiveLists(IEnumerable<TwitterList> result, TwitterResponse response)
+        {
+            lock (sync)
+            {
+                loading--;
+
+                if (loading <= 0)
+                {
+                    IsLoading = false;
+                    BarText = "";
+                    reloadLists.RaiseCanExecuteChanged();
+                }
+            }
+
+            if (response.StatusCode != HttpStatusCode.OK)
+                MessageService.ShowError(Localization.Resources.ErrorLoadingLists);
+
+            foreach (var list in result)
+                if (!Lists.Any(x => x.FullName == list.FullName))
+                    Lists.Add(list);
+        }
+
+        void AddSelectedList()
+        {
+            var list = ListSelection as TwitterList;
+            if (list == null)
+                return;
+
+            var toAdd = new TwitterResource
+            {
+                Type = ResourceType.List,
+                Data = list.FullName,
+                User = DataTransfer.CurrentAccount
+            };
+
+            SaveColumn(toAdd);           
+            ListSelection = null;
+            GoBack();        
+        }
+
+        void AddSelectedCoreResource()
+        {
+            if (CoreSelection == null || !(CoreSelection is TwitterResource))
+                return;
+
+            SaveColumn((TwitterResource)CoreSelection);
+            CoreSelection = null;
+            GoBack();
+        }
+
+        private void SaveColumn(TwitterResource toAdd)
+        {
+            if (!Config.Columns.Contains(toAdd))
+            {
+                Config.Columns.Add(toAdd);
+                Config.SaveColumns();
+                DataTransfer.ShouldReloadColumns = true;
+            }
+        }
+    }
+}
