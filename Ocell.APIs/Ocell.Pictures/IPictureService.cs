eb0891b873dd676ce04febe22a7721869f3fc959
Guillermo Julián <gjulian93@gmail.com>
2013-02-24 12:40:23 +0100

    More project restructuring

---
+++ Ocell.APIs/Ocell.Pictures/IPictureService.cs
@@ -0,0 +1,14 @@
+﻿using Hammock;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Ocell.Pictures
+{
+    public interface IPictureService
+    {
+        void SendPicture(string text, string fileName, Stream file, Action<RestResponse, string> callback);
+    }
+}
