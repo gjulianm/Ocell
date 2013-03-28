f526cccb9e7b81f2ba6f4115675fb34c695e5def
Guillermo Julián <gjulian93@gmail.com>
2013-03-28 22:34:07 +0100

    Using code from TweetView in DMView

--- Ocell.UI/Ocell.Phone7/Pages/Elements/DMView.xaml.cs
+++ Ocell.UI/Ocell.Phone7/Pages/Elements/DMView.xaml.cs
@@ -7,6 +7,12 @@ using Microsoft.Phone.Controls;
 using Microsoft.Phone.Tasks;
 using TweetSharp;
 using Ocell.Library;
+using System.Windows.Media;
+using System.Windows.Controls;
+using DanielVaughan.Services;
+using Ocell.Library.Filtering;
+using DanielVaughan;
+using Ocell.Library.Twitter;
 
 namespace Ocell.Pages.Elements
 {
@@ -25,6 +31,7 @@ namespace Ocell.Pages.Elements
         void Tweet_Loaded(object sender, RoutedEventArgs e)
         {
             RemoveHTML conv = new RemoveHTML();
+
             if (DataTransfer.DM == null)
             {
                 Dispatcher.BeginInvoke(() => MessageBox.Show(Localization.Resources.ErrorLoadingTweet));
@@ -37,43 +44,7 @@ namespace Ocell.Pages.Elements
 
             RelativeDateTimeConverter dc = new RelativeDateTimeConverter();
 
-            var paragraph = new Paragraph();
-            var runs = new List<Inline>();
-
-            Text.Blocks.Clear();
-
-            foreach (var word in status.Text.Split(' '))
-            {
-                Uri uri;
-
-                if (string.IsNullOrWhiteSpace(word))
-                    continue;
-
-                if (Uri.TryCreate(word, UriKind.Absolute, out uri) ||
-                   (word.StartsWith("www.") && Uri.TryCreate("http://" + word, UriKind.Absolute, out uri)) ||
-                    word[0]=='#' || word[0] == '@')
-                {
-                    var link = new Hyperlink();
-                    link.Inlines.Add(new Run() { Text = word });  
-                    link.FontWeight = FontWeights.Bold;
-                    link.TextDecorations = null;
-                    link.TargetName = word;
-                    link.Click += new RoutedEventHandler(link_Click);
-
-                    runs.Add(link);
-                }
-                else
-                {
-                    runs.Add(new Run() { Text = word });
-                }
-
-                runs.Add(new Run() { Text = " " });
-            }
-
-            foreach (var run in runs)
-                paragraph.Inlines.Add(run);
-            
-            Text.Blocks.Add(paragraph);
+            CreateText(status);
 
             Text.UpdateLayout();
 
@@ -87,9 +58,188 @@ namespace Ocell.Pages.Elements
             ContentPanel.UpdateLayout();
         }
 
+        private void CreateText(ITweetable Status)
+        {
+            if (Status == null)
+                return;
+
+            var paragraph = new Paragraph();
+            var runs = new List<Inline>();
+
+            Text.Blocks.Clear();
+
+            string TweetText = Status.Text;
+            string PreviousText;
+            int i = 0;
+
+            if (Status.Entities != null)
+            {
+                foreach (var Entity in Status.Entities)
+                {
+                    if (Entity.StartIndex > i)
+                    {
+                        PreviousText = TweetText.Substring(i, Entity.StartIndex - i);
+                        runs.Add(new Run { Text = HttpUtility.HtmlDecode(PreviousText) });
+                    }
+
+                    i = Entity.EndIndex;
+
+                    switch (Entity.EntityType)
+                    {
+                        case TwitterEntityType.HashTag:
+                            runs.Add(CreateHashtagLink((TwitterHashTag)Entity));
+                            break;
+
+                        case TwitterEntityType.Mention:
+                            runs.Add(CreateMentionLink((TwitterMention)Entity));
+                            break;
+
+                        case TwitterEntityType.Url:
+                            runs.Add(CreateUrlLink((TwitterUrl)Entity));
+                            break;
+                        case TwitterEntityType.Media:
+                            runs.Add(CreateMediaLink((TwitterMedia)Entity));
+                            break;
+                    }
+                }
+            }
+
+            if (i < TweetText.Length)
+                runs.Add(new Run
+                {
+                    Text = HttpUtility.HtmlDecode(TweetText.Substring(i))
+                });
+
+            foreach (var run in runs)
+                paragraph.Inlines.Add(run);
+
+            Text.Blocks.Add(paragraph);
+
+            Text.UpdateLayout();
+        }
+
+        Inline CreateBaseLink(string content, string contextHeader, string contextTag, MenuItem customButton = null)
+        {
+            var link = new HyperlinkButton
+            {
+                Content = content,
+                FontSize = Text.FontSize,
+                FontWeight = Text.FontWeight,
+                FontStretch = Text.FontStretch,
+                FontFamily = Text.FontFamily,
+                TargetName = contextTag,
+                Margin = new Thickness(-10, -5, -10, -8)
+            };
+
+            link.Click += new RoutedEventHandler(link_Click);
+
+
+            MenuItem item = new MenuItem
+            {
+                Header = contextHeader,
+                Tag = contextTag,
+                Foreground = new SolidColorBrush(Colors.Black)
+            };
+            item.Click += new RoutedEventHandler(CopyLink);
+
+            ContextMenu menu = new ContextMenu();
+            menu.Items.Add(item);
+            if (customButton != null)
+                menu.Items.Add(customButton);
+
+            ContextMenuService.SetContextMenu(link, menu);
+
+            InlineUIContainer container = new InlineUIContainer();
+            container.Child = link;
+            return container;
+        }
+
+        Inline CreateHashtagLink(TwitterHashTag Hashtag)
+        {
+            MenuItem item = new MenuItem
+            {
+                Header = Localization.Resources.MuteHashtag,
+                Foreground = new SolidColorBrush(Colors.Black)
+            };
+            item.Click += (sender, e) =>
+            {
+                var filter = FilterManager.SetupMute(FilterType.Text, "#" + Hashtag.Text);
+                Dependency.Resolve<IMessageService>().ShowMessage(String.Format(Localization.Resources.MutedUntil, filter.IsValidUntil.ToString("f")), "");
+            };
+            return CreateBaseLink("#" + Hashtag.Text, Localization.Resources.CopyHashtag, "#" + Hashtag.Text, item);
+        }
+
+        Inline CreateMentionLink(TwitterMention Mention)
+        {
+            MenuItem item = new MenuItem
+            {
+                Header = Localization.Resources.MuteUser,
+                Foreground = new SolidColorBrush(Colors.Black)
+            };
+            item.Click += (sender, e) =>
+            {
+                var filter = FilterManager.SetupMute(FilterType.User, Mention.ScreenName);
+                Dependency.Resolve<IMessageService>().ShowMessage(String.Format(Localization.Resources.MutedUntil, filter.IsValidUntil.ToString("f")), "");
+            };
+            return CreateBaseLink("@" + Mention.ScreenName, Localization.Resources.CopyUsername, "@" + Mention.ScreenName, item);
+        }
+
+        Inline CreateUrlLink(TwitterUrl URL)
+        {
+            MenuItem item = new MenuItem
+            {
+                Header = Localization.Resources.MuteDomain,
+                Foreground = new SolidColorBrush(Colors.Black)
+            };
+            item.Click += (sender, e) =>
+            {
+                Uri uri;
+                if (Uri.TryCreate(URL.ExpandedValue, UriKind.Absolute, out uri))
+                {
+                    var filter = FilterManager.SetupMute(FilterType.Text, uri.Host);
+                    Dependency.Resolve<IMessageService>().ShowMessage(String.Format(Localization.Resources.MutedUntil, filter.IsValidUntil.ToString("f")), "");
+                }
+                else
+                    Dependency.Resolve<IMessageService>().ShowError(Localization.Resources.NotValidURL);
+            };
+
+            string value = string.IsNullOrWhiteSpace(URL.ExpandedValue) ? URL.Value : URL.ExpandedValue;
+
+            return CreateBaseLink(TweetTextConverter.TrimUrl(value), Localization.Resources.CopyLink, value, item);
+        }
+
+        Inline CreateMediaLink(TwitterMedia Media)
+        {
+            MenuItem item = new MenuItem
+            {
+                Header = Localization.Resources.MuteDomain,
+                Foreground = new SolidColorBrush(Colors.Black)
+            };
+            item.Click += (sender, e) =>
+            {
+                Uri uri;
+                if (Uri.TryCreate(Media.DisplayUrl, UriKind.Absolute, out uri))
+                {
+                    var filter = FilterManager.SetupMute(FilterType.Text, uri.Host);
+                    Dependency.Resolve<IMessageService>().ShowMessage(String.Format(Localization.Resources.MutedUntil, filter.IsValidUntil.ToString("f")), "");
+                }
+                else
+                    Dependency.Resolve<IMessageService>().ShowError(Localization.Resources.NotValidURL);
+            };
+
+            return CreateBaseLink(Media.DisplayUrl, Localization.Resources.CopyLink, Media.DisplayUrl, item);
+        }
+
+        void CopyLink(object sender, RoutedEventArgs e)
+        {
+            MenuItem item = sender as MenuItem;
+            if (item != null && item.Tag is string && !(string.IsNullOrWhiteSpace(item.Tag as string)))
+                Clipboard.SetText(item.Tag as string);
+        }
+
         void link_Click(object sender, RoutedEventArgs e)
         {
-            Hyperlink link = sender as Hyperlink;
+            HyperlinkButton link = sender as HyperlinkButton;
             Uri uri;
             WebBrowserTask browser;
 
@@ -107,10 +257,16 @@ namespace Ocell.Pages.Elements
                 NavigationService.Navigate(new Uri("/Pages/Elements/User.xaml?user=" + link.TargetName.Substring(0), UriKind.Relative));
             else if (link.TargetName[0] == '#')
             {
-                DataTransfer.Search = link.TargetName;
-                NavigationService.Navigate(new Uri("/Pages/Search/Search.xaml?q=" + link.TargetName, UriKind.Relative));
+                Ocell.Pages.Search.ResourceViewModel.Resource = new TwitterResource
+                {
+                    User = DataTransfer.CurrentAccount,
+                    Type = ResourceType.Search,
+                    Data = link.TargetName
+                };
+
+                NavigationService.Navigate(Uris.ResourceView);
             }
-              
+
         }
 
         private void replyButton_Click(object sender, EventArgs e)
