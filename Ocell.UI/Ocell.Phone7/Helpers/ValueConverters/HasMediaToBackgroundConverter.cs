2a004aad9f7241a33a08ae0a13bd566786ea04ce
Guillermo Julián <gjulian93@gmail.com>
2013-09-15 19:05:53 +0200

    Added background to tweets with image.

---
+++ Ocell.UI/Ocell.Phone7/Helpers/ValueConverters/HasMediaToBackgroundConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Ocell
+{
+    public class HasMediaToBackgroundConverter : IValueConverter
+    {
+        private static TextToMediaConverter innerConverter = new TextToMediaConverter();
+        private static SolidColorBrush transparentBlack = new SolidColorBrush(Color.FromArgb(100, 0, 0, 0));
+        private static SolidColorBrush transparent = new SolidColorBrush(Colors.Transparent);
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var hasImage = innerConverter.Convert(value, targetType, parameter, culture) != null;
+
+            if (hasImage)
+                return transparentBlack;
+            else
+                return transparent;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
