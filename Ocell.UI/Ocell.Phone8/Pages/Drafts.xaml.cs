455ad6c7545e05a659be5f210e49e3dc266f781e
Guillermo Julián <gjulian93@gmail.com>
2014-09-28 22:02:29 +0200

    Draft interface redone.

---
+++ Ocell.UI/Ocell.Phone8/Pages/Drafts.xaml.cs
@@ -0,0 +1,21 @@
+﻿using AncoraMVVM.Base.ViewModelLocator;
+using Microsoft.Phone.Controls;
+using Ocell.Library.Twitter;
+using Ocell.ViewModels;
+
+namespace Ocell.Pages
+{
+    [ViewModel(typeof(DraftsModel))]
+    public partial class Drafts : PhoneApplicationPage
+    {
+        public Drafts()
+        {
+            InitializeComponent();
+
+            DraftList.SelectionChanged += (s, e) =>
+            {
+                (DataContext as DraftsModel).SelectedDraft = DraftList.SelectedItem as TwitterDraft; // LLS is crap and doesn't update the binding.
+            };
+        }
+    }
+}
---
+++ Ocell.UI/Ocell.Phone8/ViewModels/DraftsModel.cs
@@ -0,0 +1,29 @@
+﻿
+using AncoraMVVM.Base;
+using Ocell.Library.Twitter;
+using Ocell.Pages;
+namespace Ocell.ViewModels
+{
+    public class DraftsModel : ViewModelBase
+    {
+        public SafeObservable<TwitterDraft> Drafts { get; set; }
+        public TwitterDraft SelectedDraft { get; set; }
+        public DelegateCommand RemoveDraft { get; set; }
+
+        public DraftsModel()
+        {
+            RemoveDraft = new DelegateCommand((param) =>
+            {
+                var draft = param as TwitterDraft;
+                if (draft != null)
+                    Drafts.Remove(draft);
+            });
+
+            this.PropertyChanged += (s, e) =>
+            {
+                if (e.PropertyName == "SelectedDraft" && SelectedDraft != null)
+                    Navigator.MessageAndNavigate<NewTweetModel, TwitterDraft>(SelectedDraft);
+            };
+        }
+    }
+}
