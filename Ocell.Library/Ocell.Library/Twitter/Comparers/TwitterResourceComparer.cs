d3bfbfb60a81b910b44faa872a306c658790eaad
Guillermo Julián <gjulian93@gmail.com>
2015-01-16 18:57:54 +0100

    Minor reorganization in comparers.

---
+++ Ocell.Library/Ocell.Library/Twitter/Comparers/TwitterResourceComparer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ocell.Library.Twitter.Comparers
+{
+    public class TwitterResourceCompare : IEqualityComparer<TwitterResource>
+    {
+        public bool Equals(TwitterResource a, TwitterResource b)
+        {
+            return (a.User.Key == b.User.Key) && (a.Type == b.Type) && (a.Data == b.Data);
+        }
+        public int GetHashCode(TwitterResource s)
+        {
+            return s.User.Key.GetHashCode() * s.String.GetHashCode();
+        }
+    }
+}
