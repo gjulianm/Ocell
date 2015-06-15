b281a93628ff60fee235557fe748c2dc80ed78ba
Guillermo Julián <gjulianm@outlook.com>
2015-06-15 16:51:59 +0200

    Add interface for the list manager

---
+++ Ocell.UI/Ocell.Phone8/Pages/Lists/ManageLists.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace Ocell.Pages.Lists
+{
+    public partial class ManageLists : PhoneApplicationPage
+    {
+        public ManageLists()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Ocell.UI/Ocell.Phone8/SampleData/FiltersSampleData/FiltersSampleData.xaml.cs
@@ -0,0 +1,124 @@
+﻿//      *********    NO MODIFIQUE ESTE ARCHIVO     *********
+//      Este archivo se regenera mediante una herramienta de diseño.
+//       Si realiza cambios en este archivo, puede causar errores.
+namespace Expression.Blend.SampleData.FiltersSampleData
+{
+	using System; 
+	using System.ComponentModel;
+
+// Para reducir de forma significativa la superficie de los datos de ejemplo en la aplicación de producción, puede establecer
+// la constante de compilación condicional DISABLE_SAMPLE_DATA y deshabilitar los datos de ejemplo en tiempo de ejecución.
+#if DISABLE_SAMPLE_DATA
+	internal class FiltersSampleData { }
+#else
+
+	public class FiltersSampleData : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected virtual void OnPropertyChanged(string propertyName)
+		{
+			if (this.PropertyChanged != null)
+			{
+				this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+
+		public FiltersSampleData()
+		{
+			try
+			{
+				Uri resourceUri = new Uri("/Ocell;component/SampleData/FiltersSampleData/FiltersSampleData.xaml", UriKind.RelativeOrAbsolute);
+				System.Windows.Application.LoadComponent(this, resourceUri);
+			}
+			catch
+			{
+			}
+		}
+
+		private Filters _Filters = new Filters();
+
+		public Filters Filters
+		{
+			get
+			{
+				return this._Filters;
+			}
+		}
+	}
+
+	public class FiltersItem : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected virtual void OnPropertyChanged(string propertyName)
+		{
+			if (this.PropertyChanged != null)
+			{
+				this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+
+		private string _Duration = string.Empty;
+
+		public string Duration
+		{
+			get
+			{
+				return this._Duration;
+			}
+
+			set
+			{
+				if (this._Duration != value)
+				{
+					this._Duration = value;
+					this.OnPropertyChanged("Duration");
+				}
+			}
+		}
+
+		private string _Filter = string.Empty;
+
+		public string Filter
+		{
+			get
+			{
+				return this._Filter;
+			}
+
+			set
+			{
+				if (this._Filter != value)
+				{
+					this._Filter = value;
+					this.OnPropertyChanged("Filter");
+				}
+			}
+		}
+
+		private string _IsValidUntil = string.Empty;
+
+		public string IsValidUntil
+		{
+			get
+			{
+				return this._IsValidUntil;
+			}
+
+			set
+			{
+				if (this._IsValidUntil != value)
+				{
+					this._IsValidUntil = value;
+					this.OnPropertyChanged("IsValidUntil");
+				}
+			}
+		}
+	}
+
+	public class Filters : System.Collections.ObjectModel.ObservableCollection<FiltersItem>
+	{ 
+	}
+#endif
+}
