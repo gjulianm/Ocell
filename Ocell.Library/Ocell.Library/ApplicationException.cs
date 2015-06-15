d82d1a691c44c2a7f9dbff7aaa1233a816dc900b
Guillermo Julián <gjulianm@outlook.com>
2015-06-15 18:07:57 +0200

    Add a general "ApplicationException" class to improve use of exceptions

---
+++ Ocell.Library/Ocell.Library/ApplicationException.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using TweetSharp;
+
+namespace Ocell.Library
+{
+    public class ApplicationException : Exception
+    {
+        public virtual TwitterError TwitterError { get; set; }
+
+        public ApplicationException(string message)
+            : base(message)
+        {
+        }
+
+        public ApplicationException(string message, TwitterError error)
+            : base(message)
+        {
+            TwitterError = error;
+        }
+
+        public override string Message
+        {
+            get
+            {
+                if (TwitterError != null)
+                    return String.Format("{0}: TwitterError - {1}", base.Message, TwitterError.Message);
+                else
+                    return base.Message;
+            }
+        }
+    }
+}
