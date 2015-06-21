acd45ab40c6dc8ac191639840a2c91e035aa8a92
Guillermo Julián <gjulianm@outlook.com>
2015-06-21 20:35:48 +0200

    Create the UserProviderDispatcher

---
+++ Ocell.Library/Ocell.Library/RuntimeData/BaseDispatcher.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Ocell.Library.Twitter;
+
+namespace Ocell.Library.RuntimeData
+{
+    public abstract class BaseDispatcher<T>
+    {
+        private Dictionary<UserToken, T> items = new Dictionary<UserToken, T>();
+        private object lockFlag = new object();
+
+        protected T CreateAndSaveItem(UserToken token)
+        {
+            T item = CreateNewItem(token);
+
+            lock (lockFlag)
+            {
+                items[token] = item;
+            }
+
+            return item;
+        }
+
+        public virtual T GetForUser(UserToken token)
+        {
+            if (token == null || token.Key == null)
+                throw new ApplicationException("Can't get a service for a null token.");
+
+            lock (lockFlag)
+            {
+                T item;
+
+                if (items.TryGetValue(token, out item))
+                    return item;
+
+                return CreateAndSaveItem(token);
+            }
+        }
+
+        public virtual T GetDefault()
+        {
+            return GetForUser(Config.Accounts.Value.FirstOrDefault());
+        }
+
+        public void CreateForAllAccounts()
+        {
+            foreach (var account in Config.Accounts.Value)
+                CreateAndSaveItem(account);
+        }
+
+        protected abstract T CreateNewItem(UserToken token);
+    }
+}
---
+++ Ocell.Library/Ocell.Library/RuntimeData/UserProviderDispatcher.cs
@@ -0,0 +1,15 @@
+﻿using Ocell.Library.Twitter;
+
+namespace Ocell.Library.RuntimeData
+{
+    public class UserProviderDispatcher : BaseDispatcher<UserProvider>
+    {
+        protected override UserProvider CreateNewItem(UserToken token)
+        {
+            var provider = new UserProvider { User = token };
+            provider.Start();
+
+            return provider;
+        }
+    }
+}
